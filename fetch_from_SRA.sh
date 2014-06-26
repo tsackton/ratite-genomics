73fe41605e13c126ef4c3168c0e220dd842327aa
Tim Sackton <tsackton@oeb.harvard.edu>
2014-06-26 09:47:58 -0400

    Update SRA script, add SRA metadata files

--- fetch_from_SRA.sh
+++ fetch_from_SRA.sh
@@ -1,3 +1,21 @@
 #!/bin/bash
 
 #script to fetch existing data from SRA for selected initial species
+#written for the Harvard Odyssey cluster module management system and SLURM
+
+#get aspera
+module load hpc/aspera
+
+#set up directories
+mkdir -p /n/regal/edwards_lab/ratites/data/sra/strCam
+mkdir -p /n/regal/edwards_lab/ratites/data/sra/tinGut
+mkdir -p /n/regal/edwards_lab/ratites/data/sra/melUnd
+
+#fetch ostrich
+ascp -i /n/sw/aspera/etc/asperaweb_id_dsa.putty -k1 -QTr –l200m anonftp@ftp-trace.ncbi.nlm.nih.gov/sra/sra-instant/reads/ByStudy/sra/SRP/SRP028/SRP028745/*/*.sra /n/regal/edwards_lab/ratites/data/sra/strCam
+
+#fetch tinamou
+ascp -i /n/sw/aspera/etc/asperaweb_id_dsa.putty -k1 -QTr –l200m anonftp@ftp-trace.ncbi.nlm.nih.gov/sra/sra-instant/reads/ByStudy/sra/SRP/SRP028/SRP028753/*/*.sra /n/regal/edwards_lab/ratites/data/sra/tinGut
+
+#fetch budgie
+ascp -i /n/sw/aspera/etc/asperaweb_id_dsa.putty -k1 -QTr –l200m anonftp@ftp-trace.ncbi.nlm.nih.gov/sra/sra-instant/reads/ByStudy/sra/ERP/ERP002/ERP002324/*/*.sra n/regal/edwards_lab/ratites/data/sra/melUnd
