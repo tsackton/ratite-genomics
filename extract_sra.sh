8f6e0883970d06ea6324d52f9b45dd43bc1b70a1
Tim Sackton <tsackton@oeb.harvard.edu>
2014-07-01 09:29:22 -0400

    Final exract_sra update

--- extract_sra.sh
+++ extract_sra.sh
@@ -6,15 +6,15 @@
 #SBATCH -o extsra_%j.out
 #SBATCH -e extsra_%j.err
 #SBATCH -p serial_requeue
-#SBATCH -t 3:00:00
-#SBATCH --array 0-7
+#SBATCH -t 9:00:00
+#SBATCH --array 0-8
 
 #extracts SRA files using a job array and a bash index
 #move to the directory containing SRA files, and change the job array size as necessary
 
 module load bio/sratoolkit.2.3.3-4
 
-declare -a FILES=$(ls *.sra)
-SRA=$FILES[$SLURM_ARRAY_TASK_ID]
+FILES=($(ls *.sra))
+SRA=${FILES[$SLURM_ARRAY_TASK_ID]}
 fastq-dump --split-files --gzip $SRA
 rm $SRA
