53da61d0d32c9d71f4fb8d313050f75091358d1a
Tim Sackton <tsackton@oeb.harvard.edu>
2014-08-19 22:10:30 -0400

    Add AllPaths prepare reads script and files

---
+++ prep_allpaths_runs.sh
@@ -0,0 +1,4 @@
+PrepareAllPathsInputs.pl DATA_DIR=/scratch/tsackton/allpaths_runs/aptOwe/RAWSEQ PLOIDY=2 IN_GROUPS_CSV=aptOwe_groups.csv IN_LIBS_CSV=aptOwe_libs.csv 1> aptOwe_prep.log 2> aptOwe_prep.err
+PrepareAllPathsInputs.pl DATA_DIR=/scratch/tsackton/allpaths_runs/casCas/RAWSEQ PLOIDY=2 IN_GROUPS_CSV=casCas_groups.csv IN_LIBS_CSV=casCas_libs.csv 1> casCas_prep.log 2> casCas_prep.err
+PrepareAllPathsInputs.pl DATA_DIR=/scratch/tsackton/allpaths_runs/droNov/RAWSEQ PLOIDY=2 IN_GROUPS_CSV=droNov_groups.csv IN_LIBS_CSV=droNov_libs.csv 1> droNov_prep.log 2> droNov_prep.err
+PrepareAllPathsInputs.pl DATA_DIR=/scratch/tsackton/allpaths_runs/rheAme/RAWSEQ PLOIDY=2 IN_GROUPS_CSV=rheAme_groups.csv IN_LIBS_CSV=rheAme_libs.csv 1> rheAme_prep.log 2> rheAme_prep.err
