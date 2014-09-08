26e2eba669790167f33a1cf59e8676e4d7232c56
Tim Sackton <tsackton@oeb.harvard.edu>
2014-09-08 12:31:36 -0400

    REAPR scripts

--- run_busco.sh
+++ run_busco.sh
@@ -4,6 +4,6 @@ ASSEMBLYPATH=$1
 SPEC=$2
 cp $ASSEMBLYPATH ${SPEC}_genome.fas
 makeblastdb -dbtype nucl -in ${SPEC}_genome.fas -parse_seqids 
-tblastn -db ${SPEC}_genome.fas -query BUSCO_GGALL_refined.fas -evalue 0.1 -out ${SPEC}_refined.blastout.txt -outfmt 6 -seg no -num_threads 24
-perl BUSCO.pl --run_refined $SPEC DMELA
+tblastn -db ${SPEC}_genome.fas -query BUSCO_GGALL_refined.fas -evalue 0.1 -out ${SPEC}_refined.blastout.txt -outfmt 6 -seg no -num_threads 8
+perl BUSCO.pl --run_refined $SPEC GGALL
 
---
+++ run_reapr.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#REAPR PIPELINE
+
+#SETUP
+SPEC=$1
+mkdir -p /scratch/tsackton/assemblies/$SPEC/reapr
+cd /scratch/tsackton/assemblies/$SPEC/reapr
+ASSEMBLY="../haplofied/final.assembly.fa"
+LOCAL_AS="$SPEC.fa"
+
+#CHECK FASTA AND MAKE LOCAL COPY OF ASSEMBLY
+reapr facheck $ASSEMBLY $SPEC
+reapr perfectmap $LOCAL_AS /scratch/tsackton/ratite_trimmed/reapr/${SPEC}_frag_1_forREAPR.fastq.gz /scratch/tsackton/ratite_trimmed/reapr/${SPEC}_frag_2_forREAPR.fastq.gz 280 ${SPEC}_perfect 
+reapr smaltmap -n 16 $LOCAL_AS /scratch/tsackton/ratite_trimmed/reapr/${SPEC}_jump_1_forREAPR.fastq.gz /scratch/tsackton/ratite_trimmed/reapr/${SPEC}_jump_2_forREAPR.fastq.gz ${SPEC}_longmapped.bam
+reapr pipeline $LOCAL_AS ${SPEC}_longmapped.bam ../final ${SPEC}_perfect
