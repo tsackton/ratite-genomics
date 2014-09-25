b8087f7dec4546d02c32dc05a2e78222489c8745
Tim Sackton <tsackton@oeb.harvard.edu>
2014-09-25 15:18:18 -0400

    Add BWA mapping scripts

---
+++ merge_reads_for_mapping.sh
@@ -0,0 +1,20 @@
+#aptOwe
+cat ../data/HighOutput/aptOwe_R1_ho_220_1P.fastq.gz ../data/initRapidRun/aptOwe220.trim_1P.fastq.gz > aptOwe_frag_R1.fastq.gz
+cat ../data/HighOutput/aptOwe_R1_ho_220_2P.fastq.gz ../data/initRapidRun/aptOwe220.trim_2P.fastq.gz > aptOwe_frag_R2.fastq.gz
+cat ../data/HighOutput/aptOwe_R1_ho_3kb_1P.fastq.gz ../data/initRapidRun/aptOwe3kb.trim_1P.fastq.gz > aptOwe_jump_R1.fastq.gz
+cat ../data/HighOutput/aptOwe_R1_ho_3kb_2P.fastq.gz ../data/initRapidRun/aptOwe3kb.trim_2P.fastq.gz > aptOwe_jump_R2.fastq.gz
+#droNov
+cat ../data/HighOutput/droNov_R1_ho_220_1P.fastq.gz ../data/initRapidRun/droNov220.trim_1P.fastq.gz > droNov_frag_R1.fastq.gz
+cat ../data/HighOutput/droNov_R1_ho_220_2P.fastq.gz ../data/initRapidRun/droNov220.trim_2P.fastq.gz > droNov_frag_R2.fastq.gz
+cat ../data/HighOutput/droNov_R1_ho_3kb_1P.fastq.gz ../data/initRapidRun/droNov3kb.trim_1P.fastq.gz > droNov_jump_R1.fastq.gz
+cat ../data/HighOutput/droNov_R1_ho_3kb_2P.fastq.gz ../data/initRapidRun/droNov3kb.trim_2P.fastq.gz > droNov_jump_R2.fastq.gz
+#casCas
+cat ../data/HighOutput/casCas_R1_ho_220_1P.fastq.gz ../data/initRapidRun/casCas220.trim_1P.fastq.gz > casCas_frag_R1.fastq.gz
+cat ../data/HighOutput/casCas_R1_ho_220_2P.fastq.gz ../data/initRapidRun/casCas220.trim_2P.fastq.gz > casCas_frag_R2.fastq.gz
+cat ../data/HighOutput/casCas_R1_ho_3kb_1P.fastq.gz ../data/initRapidRun/casCas3kb.trim_1P.fastq.gz > casCas_jump_R1.fastq.gz
+cat ../data/HighOutput/casCas_R1_ho_3kb_2P.fastq.gz ../data/initRapidRun/casCas3kb.trim_2P.fastq.gz > casCas_jump_R2.fastq.gz
+#rheAme
+cat ../data/HighOutput/rheAme_R1_ho_220_1P.fastq.gz ../data/initRapidRun/rheAme220.trim_1P.fastq.gz > rheAme_frag_R1.fastq.gz
+cat ../data/HighOutput/rheAme_R1_ho_220_2P.fastq.gz ../data/initRapidRun/rheAme220.trim_2P.fastq.gz > rheAme_frag_R2.fastq.gz
+cat ../data/HighOutput/rheAme_R1_ho_3kb_1P.fastq.gz ../data/initRapidRun/rheAme3kb.trim_1P.fastq.gz > rheAme_jump_R1.fastq.gz
+cat ../data/HighOutput/rheAme_R1_ho_3kb_2P.fastq.gz ../data/initRapidRun/rheAme3kb.trim_2P.fastq.gz > rheAme_jump_R2.fastq.gz
