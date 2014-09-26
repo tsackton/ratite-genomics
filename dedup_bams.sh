9a6ffa888cc2d33041d79028d3210555875aa47e
Tim Sackton <tsackton@oeb.harvard.edu>
2014-09-26 12:02:00 -0400

    Add dedup script

--- dedup_bams.sh
+++ dedup_bams.sh
@@ -15,8 +15,8 @@
 
 SPEC=$1
 
-java -jar ~/sw/progs/picard-tools-1.121/SortSam.jar INPUT=${SPEC}_frag_mapped.bam OUTPUT=${SPEC}_sorted1.bam
-java -jar ~/sw/progs/picard-tools-1.121/SortSam.jar INPUT=${SPEC}_jump_mapped.bam OUTPUT=${SPEC}_sorted2.bam
+java -jar ~/sw/progs/picard-tools-1.121/SortSam.jar INPUT=${SPEC}_frag_mapped.bam OUTPUT=${SPEC}_sorted1.bam SORT_ORDER=coordinate
+java -jar ~/sw/progs/picard-tools-1.121/SortSam.jar INPUT=${SPEC}_jump_mapped.bam OUTPUT=${SPEC}_sorted2.bam SORT_ORDER=coordinate
 java -jar ~/sw/progs/picard-tools-1.121/MarkDuplicates.jar INPUT=${SPEC}_sorted1.bam OUTPUT=${SPEC}_dedup1.bam METRICS_FILE=${SPEC}_metrics1.txt REMOVE_DUPLICATES=true
 java -jar ~/sw/progs/picard-tools-1.121/MarkDuplicates.jar INPUT=${SPEC}_sorted2.bam OUTPUT=${SPEC}_dedup2.bam METRICS_FILE=${SPEC}_metrics2.txt REMOVE_DUPLICATES=true
 java -jar ~/sw/progs/picard-tools-1.121/MergeSamFiles.jar INPUT=${SPEC}_dedup1.bam INPUT=${SPEC}_dedup2.bam OUTPUT=${SPEC}_clean.bam
