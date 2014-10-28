948a3577f0894e9d3cfe0804e113a9ba2c434271
Tim Sackton <tsackton@oeb.harvard.edu>
2014-10-28 16:12:53 -0400

    Next two species

--- prep_allpaths_runs.sh
+++ prep_allpaths_runs.sh
@@ -1,4 +1,8 @@
-PrepareAllPathsInputs.pl DATA_DIR=/scratch/tsackton/allpaths_runs/aptOwe/RAWSEQ PLOIDY=2 IN_GROUPS_CSV=aptOwe_groups.csv IN_LIBS_CSV=aptOwe_libs.csv 1> aptOwe_prep.log 2> aptOwe_prep.err
-PrepareAllPathsInputs.pl DATA_DIR=/scratch/tsackton/allpaths_runs/casCas/RAWSEQ PLOIDY=2 IN_GROUPS_CSV=casCas_groups.csv IN_LIBS_CSV=casCas_libs.csv 1> casCas_prep.log 2> casCas_prep.err
-PrepareAllPathsInputs.pl DATA_DIR=/scratch/tsackton/allpaths_runs/droNov/RAWSEQ PLOIDY=2 IN_GROUPS_CSV=droNov_groups.csv IN_LIBS_CSV=droNov_libs.csv 1> droNov_prep.log 2> droNov_prep.err
-PrepareAllPathsInputs.pl DATA_DIR=/scratch/tsackton/allpaths_runs/rheAme/RAWSEQ PLOIDY=2 IN_GROUPS_CSV=rheAme_groups.csv IN_LIBS_CSV=rheAme_libs.csv 1> rheAme_prep.log 2> rheAme_prep.err
+#!/bin/bash
+
+#PrepareAllPathsInputs.pl DATA_DIR=/scratch/tsackton/allpaths_runs/aptOwe/RAWSEQ PLOIDY=2 IN_GROUPS_CSV=aptOwe_groups.csv IN_LIBS_CSV=aptOwe_libs.csv 1> aptOwe_prep.log 2> aptOwe_prep.err
+#PrepareAllPathsInputs.pl DATA_DIR=/scratch/tsackton/allpaths_runs/casCas/RAWSEQ PLOIDY=2 IN_GROUPS_CSV=casCas_groups.csv IN_LIBS_CSV=casCas_libs.csv 1> casCas_prep.log 2> casCas_prep.err
+#PrepareAllPathsInputs.pl DATA_DIR=/scratch/tsackton/allpaths_runs/droNov/RAWSEQ PLOIDY=2 IN_GROUPS_CSV=droNov_groups.csv IN_LIBS_CSV=droNov_libs.csv 1> droNov_prep.log 2> droNov_prep.err
+#PrepareAllPathsInputs.pl DATA_DIR=/scratch/tsackton/allpaths_runs/rheAme/RAWSEQ PLOIDY=2 IN_GROUPS_CSV=rheAme_groups.csv IN_LIBS_CSV=rheAme_libs.csv 1> rheAme_prep.log 2> rheAme_prep.err
+PrepareAllPathsInputs.pl DATA_DIR=/scratch/tsackton/allpaths_runs/aptHaa/RAWSEQ PLOIDY=2 IN_GROUPS_CSV=aptHaa_groups.csv IN_LIBS_CSV=aptHaa_libs.csv 1> aptHaa_prep.log 2> aptHaa_prep.err
+PrepareAllPathsInputs.pl DATA_DIR=/scratch/tsackton/allpaths_runs/cryCin/RAWSEQ PLOIDY=2 IN_GROUPS_CSV=cryCin_groups.csv IN_LIBS_CSV=cryCin_libs.csv 1> cryCin_prep.log 2> cryCin_prep.err
