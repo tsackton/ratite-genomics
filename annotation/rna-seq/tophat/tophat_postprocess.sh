3f13c0a880f71fad162e68c44221895502042950
Tim Sackton <tsackton@oeb.harvard.edu>
2015-08-13 12:14:21 -0400

    Updating tophat_postprocess.sh

--- annotation/rna-seq/tophat/tophat_postprocess.sh
+++ annotation/rna-seq/tophat/tophat_postprocess.sh
@@ -4,6 +4,9 @@
 #merge alt-species into one GFF
 #keep same-species (droNov/droNov and notPer/notPer) independent
 
+mkdir output_for_maker
+cd output_for_maker
+
 #first filter with awk
 for BED in $(ls ../*/junctions.bed);
 do
@@ -31,10 +34,15 @@ cp notPer.notper.junctions.bed.gff notPer.est.gff
 cp cryCin.notper.junctions.bed.gff cryCin.altest.gff
 cp eudEle.notper.junctions.bed.gff eudEle.altest.gff
 
-#kiwi, cassowary, rheas have just altest of both kiwi and notper
-gff3_merge -o aptHaa.altest.gff aptHaa*.junctions.bed.gff
-gff3_merge -o aptRow.altest.gff aptRow*.junctions.bed.gff
-gff3_merge -o aptOwe.altest.gff aptOwe*.junctions.bed.gff
+#kiwi have both
+cp aptHaa.kiwi.junctions.bed.gff aptHaa.est.gff
+cp aptHaa.dronov.junctions.bed.gff aptHaa.altest.gff
+cp aptRow.kiwi.junctions.bed.gff aptRow.est.gff
+cp aptRow.dronov.junctions.bed.gff aptRow.altest.gff
+cp aptOwe.kiwi.junctions.bed.gff aptOwe.est.gff
+cp aptOwe.dronov.junctions.bed.gff aptOwe.altest.gff
+
+#cassowary, rheas have just altest of both kiwi and notper
 gff3_merge -o casCas.altest.gff casCas*.junctions.bed.gff
 gff3_merge -o rheAme.altest.gff rheAme*.junctions.bed.gff
 gff3_merge -o rhePen.altest.gff rhePen*.junctions.bed.gff
