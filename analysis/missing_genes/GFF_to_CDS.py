f9ae35f80ba74a53126e92d9e22f5d6a68ce92f1
phil-grayson <pgrayson@fas.harvard.edu>
2015-09-16 17:16:24 -0400

    Found error in GFF_to_CDS and simplified the file substantially

--- analysis/missing_genes/GFF_to_CDS.py
+++ analysis/missing_genes/GFF_to_CDS.py
@@ -6,19 +6,14 @@ This file reads through an NCBI gff and creates a new gff with only the lines th
 """
 
 import csv
-import re
 
-file_out = '/Users/Phil/Desktop/PseudoSearch/Chicken_CDS_Only.gff'
+file_out = '/Users/Phil/Desktop/PseudoSearch/Chicken_CDS.gff'
 fout = open(file_out, 'w')
 with open('/Users/Phil/Desktop/PseudoSearch/GCF_000002315.3_Gallus_gallus-4.0_genomic.gff', 'rU') as handle: #opens gff in universal mode
     reader=csv.reader(handle,delimiter='\t') #reads file with tabs as delimiters
-    for lll in reader:
-        if len(lll)==9: #only the lines in proper gff format (length of 9) should be examined
-            pcsearch = re.search(r'GeneID\:(\d*)\;.*gene_biotype=protein_coding', lll[8]) #find lines with biotype=protein_coding and records as pcsearch
-            if bool(pcsearch): #if protein coding biotype in line
-                pcmatch = pcsearch.group(1) #call pcmatch the gene id of that gene
-            if lll[2]=="CDS" and str(pcmatch) in lll[8]: #if a given line in file has CDS in column 3 and gene id matches in column 9...
-                llll = '\t'.join(lll) #turn that line (which is in list format) into a tab delimited string
-                fout.write(llll + '\n') #and write that string out
+    for strLine in reader:
+        if len(strLine)==9: #only the lines in proper gff format (length of 9) should be examined
+            if strLine[2]=="CDS": #if a given line in file has CDS in column 3 ...
+                strLine2 = '\t'.join(strLine) #turn that line (which is in list format) into a tab delimited string
+                fout.write(strLine2 + '\n') #and write that string out
 fout.close()
-
