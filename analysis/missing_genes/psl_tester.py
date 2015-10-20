e269c4caf71910e8d8ba6860e33c34b0f86f94d4
phil-grayson <pgrayson@fas.harvard.edu>
2015-10-20 11:31:50 -0400

    commenting psl_parser as I work and writing a separate file for psl_tester

--- analysis/missing_genes/psl_tester.py
+++ analysis/missing_genes/psl_tester.py
@@ -1,5 +1,4 @@
 import csv
-#from Bio import SeqIO
 import os
 import re
 total = 0 #a bunch of counts that start at 0
@@ -9,9 +8,6 @@ falseStart = 0
 falseEnd = 0
 falseBoth = 0
 
-#fout = open('/Users/Phil/Desktop/messingaroundfulGla.txt','w')
-#os.chdir('/Users/Phil/Desktop/')
-#with open('/Users/Phil/Desktop/fulGla.psl', 'rU') as handle: #opens bed in universal mode
 fout = open('/n/regal/edwards_lab/phil/PseudoSearch/Final/HLO_psl/counts/'+os.getenv('FILE')+'.text','w') #out files will be ${FILE}.txt
 with open('/n/regal/edwards_lab/phil/PseudoSearch/Final/HLO_psl/'+os.getenv('FILE'), 'rU') as handle:
     reader=csv.reader(handle,delimiter='\t') #reads file with tabs as delimiters
@@ -59,57 +55,3 @@ fout.write("\n"+"total is "+str(total)+" and components add to "+str(components)
 if int(total) != int(components): #if the total and the sum of components are not equal
     fout.write("\n"+"We've got a problem here"+"\n") #write this message
 fout.close()
-
-#            if strand == "+-":
-#                rc = True
-#            elif strand == "--":
-#                rc = True
-#            else:
-#                rc = False
-#            qExonSize=qEnd-qStart
-#            tExonSize=tEnd-tStart
-#            if qExonSize==tExonSize:
-#                #genome=os.getenv('1')#link to $1 in batch script
-#                #genome=str(file)+".fa"
-#                genome='fulGla.fa'
-#                z = SeqIO.index_db(genome+".idx", genome, "fasta")
-#                z = z.get(tName)[tStart:tEnd] #returns the target exon from the target scaffold as a SeqRecord 
-#                fasta = z.format("fasta") #returns the SeqRecord in fasta format
-#                if rc:
-#                    zrc=(z.reverse_complement(id="rc_",description=z.description))
-#                    rcfasta=zrc.format("fasta")
-#                    fout.write(rcfasta)
-#                else:
-#                    fout.write(fasta)
-#    fout.close()
-            
-        
-#        else:
-#            #here is where we write the n's in.
-#        if int(qnamestart.group(1)) != qStart:
-#            print "Start is off for "+str(strLine)
-#        if int(qnamestop.group(1)) != qEnd:
-#            print "End is off for "+str(strLine)
-#        
-#        if strand == "+-":
-#            rc = True
-#        elif strand == "--":
-#            rc = True
-#        else:
-#            rc = False
-#        qExonSize=qEnd-qStart
-#        tExonSize=tEnd-tStart
-#        if qExonSize==tExonSize:
-#            #genome=os.getenv('1')#link to $1 in batch script
-#            #genome=str(file)+".fa"
-#            genome='fulGla.fa'
-#            z = SeqIO.index_db(genome+".idx", genome, "fasta")
-#            z = z.get(tName)[tStart:tEnd] #returns the target exon from the target scaffold as a SeqRecord 
-#            fasta = z.format("fasta") #returns the SeqRecord in fasta format
-#            if rc:
-#                zrc=(z.reverse_complement(id="rc_",description=z.description))
-#                rcfasta=zrc.format("fasta")
-#                fout.write(rcfasta)
-#            else:
-#                fout.write(fasta)
-#    fout.close()
