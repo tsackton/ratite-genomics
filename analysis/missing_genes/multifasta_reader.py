5949e4527be6948d285b27342dd957f0804a2dce
phil-grayson <pgrayson@fas.harvard.edu>
2015-10-29 12:24:27 -0400

    now have working fasta splitter within multifasta_reader.py.  Next step is to isolate longest reading frame and determine properties of that ORF.

--- analysis/missing_genes/multifasta_reader.py
+++ analysis/missing_genes/multifasta_reader.py
@@ -2,73 +2,45 @@
 """
 Created on Mon Oct 26 17:10:11 2015
 
-@author: Phil
+@author: phil-grayson
 """
-
 from Bio import SeqIO
 from Bio.Seq import Seq
 from Bio.Seq import Seq
 from Bio.SeqRecord import SeqRecord
+star = "*"
+star = (Seq(star))
+ts_ed = []
+nts_ed = []
 
 infile = '/Users/Phil/Desktop/test.txt'
 for record in SeqIO.parse(infile,"fasta"):
     ts_ed = [] #trans_split (empty dropped) 
-    trans_split = record.seq.translate().split('*')
-    print trans_split
-    print len(trans_split)    
-    if len(trans_split) == 2:
+    trans_split = record.seq.translate().split('*')      
+    #print trans_split    
+    if len(trans_split[-1]) == 0 and len(trans_split[0]) == 0:
+        trans_split.pop(-1)
+        trans_split.pop(0)
+        rf = trans_split[-1]+star
+    elif len(trans_split[-1]) == 0 and len(trans_split[0]) != 0:
         trans_split.pop(1)
-        print trans_split
+        rf = trans_split[-1]+star
+    elif len(trans_split[-1]) != 0 and len(trans_split[0]) == 0:
+        trans_split.pop(0)           
+        rf = trans_split[-1]
     else:
-        end = len(trans_split)
-        if len(trans_split[end-1]) == 0:
-            print trans_split
-
-    for orf in trans_split:      
-        norf = orf+star
-        ts_ed.append(norf)
-    print ts_ed 
-    print trans_split       
-        
-        if len(orf) > 0:
-            ts_ed.append(orf)
-    end = len(ts_ed)
-    star = "*"
-    star = (Seq(star))
-    nts_ed = []    
-    for norf in ts_ed[0:end-1]:
-        norf = norf+star
-        nts_ed.append(norf)
+        rf = trans_split[-1]
+    for orf in trans_split[0:-1]: 
+        orf = orf+star
+        nts_ed.append(orf)
+    nts_ed.append(rf)
     print nts_ed
+    nts_ed=[]
     
 
-infile = '/Users/Phil/Desktop/test.txt'
-for record in SeqIO.parse(infile,"fasta"):
-    ts_ed = [] #trans_split (empty dropped) 
-    trans_split = record.seq.translate().split('*')
-    for orf in trans_split:      
-        if len(orf) > 0:
-            ts_ed.append(orf)
-    end = len(ts_ed)
-    star = "*"
-    star = SeqRecord(Seq(star))
-    nts_ed = []    
-    for norf in ts_ed[0:end-1]:
-        norf = norf+star
-        nts_ed.append(norf)
-    print nts_ed
 
 
-
-
-
-
-    
     for orf in ts_ed
     print max(mylist, key=len)
 
-print trec
-print strec
-for rec in strec:
-    print rec
 
