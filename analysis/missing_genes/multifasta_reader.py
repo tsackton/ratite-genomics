ced5e110b587c18fd7ea5fd23bca16154188b46b
phil-grayson <pgrayson@fas.harvard.edu>
2015-10-29 16:30:42 -0400

    multifasta_reader.py update for multiple stop codons at beginning or end of sequence

--- analysis/missing_genes/multifasta_reader.py
+++ analysis/missing_genes/multifasta_reader.py
@@ -6,41 +6,35 @@ Created on Mon Oct 26 17:10:11 2015
 """
 from Bio import SeqIO
 from Bio.Seq import Seq
-from Bio.Seq import Seq
-from Bio.SeqRecord import SeqRecord
+
 star = "*"
 star = (Seq(star))
 ts_ed = []
-nts_ed = []
-
+fout = open('/Users/Phil/Desktop/testout.txt','w')
 infile = '/Users/Phil/Desktop/test.txt'
 for record in SeqIO.parse(infile,"fasta"):
-    ts_ed = [] #trans_split (empty dropped) 
-    trans_split = record.seq.translate().split('*')      
-    #print trans_split    
-    if len(trans_split[-1]) == 0 and len(trans_split[0]) == 0:
-        trans_split.pop(-1)
-        trans_split.pop(0)
-        rf = trans_split[-1]+star
-    elif len(trans_split[-1]) == 0 and len(trans_split[0]) != 0:
-        trans_split.pop(1)
-        rf = trans_split[-1]+star
-    elif len(trans_split[-1]) != 0 and len(trans_split[0]) == 0:
-        trans_split.pop(0)           
-        rf = trans_split[-1]
-    else:
-        rf = trans_split[-1]
-    for orf in trans_split[0:-1]: 
-        orf = orf+star
-        nts_ed.append(orf)
-    nts_ed.append(rf)
-    print nts_ed
-    nts_ed=[]
-    
-
-
-
-    for orf in ts_ed
-    print max(mylist, key=len)
-
-
+    trans_split = record.seq.translate().split('*') #translate the sequence and split on stops    
+    if len(trans_split[-1]) != 0 and len(trans_split[0]) != 0: #if there was not a stop at the beginning or end of the translated protein
+        rf = trans_split[-1] #call the last reading frame rf
+    if len(trans_split[0]) == 0: #if there are any stop codons at the beginning of the sequences
+        try: #necessary because an entire sequence of stop codons would throw an index error
+            while len(trans_split[0]) == 0: #while there are stop codons at the beginning of the sequence
+                trans_split.pop(0) #pop them off
+            rf = trans_split[-1] #once the stops are gone, call the last reading frame as rf
+        except:
+            rf = "*" #if the entire sequence was stop codons, call the last reading frame "*"
+    try: #as above, necessary in case the whole sequence was stop codons
+        if len(trans_split[-1]) == 0: #if you have stop codons at the end of the sequence
+            while len(trans_split[-1]) == 0: #while there are stop codons at the end of the sequence
+                trans_split.pop(-1) #pop them off
+            rf = trans_split[-1]+star #once the stops are gone, add one back to the final piece of sequence
+    except:
+        None #do nothing since rf is already "*" from above
+    for orf in trans_split[0:-1]: #for every reading frame for a given transcript (except the final one)
+        orf = orf+star #add "*" back, since we've split it off
+        ts_ed.append(orf) #to a new list, add these pieces in order
+    ts_ed.append(rf) #and once that is complete, add on the final reading frame (either "*", seq with "*", or seq without "*" based on biology)
+    longest = max(ts_ed, key=len) #call the longest reading frame for a given transcript longest
+    ts_ed=[]
+    fout.write(">"+str(record.description)+"\n"+str(longest)+"\n") #and write this out with appropriate records
+fout.close()
