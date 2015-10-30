30484a6bdd13f1a95fb42570d021a39135b7472f
phil-grayson <pgrayson@fas.harvard.edu>
2015-10-30 12:58:10 -0400

    update to multifasta_reader.py and creation of translation_tester.py which borrows heavily from psl_tester.py

--- analysis/missing_genes/multifasta_reader.py
+++ analysis/missing_genes/multifasta_reader.py
@@ -10,10 +10,14 @@ from Bio.Seq import Seq
 star = "*"
 star = (Seq(star))
 ts_ed = []
-fout = open('/Users/Phil/Desktop/testout.txt','w')
-infile = '/Users/Phil/Desktop/test.txt'
+fout = open('/Users/Phil/Desktop/testoutgG_orfs.txt','w')
+fout2 = open('/Users/Phil/Desktop/testoutgG_longest.txt','w')
+fout3 = open('/Users/Phil/Desktop/testoutgG_trans.txt','w')
+infile = '/Users/Phil/Desktop/concat_galGal.fa'
 for record in SeqIO.parse(infile,"fasta"):
-    trans_split = record.seq.translate().split('*') #translate the sequence and split on stops    
+    trans = record.seq.translate()    
+    trans_split = trans.split('*') #translate the sequence and split on stops  
+    orfs = len(trans_split)
     if len(trans_split[-1]) != 0 and len(trans_split[0]) != 0: #if there was not a stop at the beginning or end of the translated protein
         rf = trans_split[-1] #call the last reading frame rf
     if len(trans_split[0]) == 0: #if there are any stop codons at the beginning of the sequences
@@ -36,5 +40,9 @@ for record in SeqIO.parse(infile,"fasta"):
     ts_ed.append(rf) #and once that is complete, add on the final reading frame (either "*", seq with "*", or seq without "*" based on biology)
     longest = max(ts_ed, key=len) #call the longest reading frame for a given transcript longest
     ts_ed=[]
-    fout.write(">"+str(record.description)+"\n"+str(longest)+"\n") #and write this out with appropriate records
+    fout.write(str(record.description)+"\t"+str(orfs)+"\n")    
+    fout2.write(">"+str(record.description)+"\n"+str(longest)+"\n") #and write this out with appropriate records
+    fout3.write(">"+str(record.description)+"\n"+str(trans)+"\n")
 fout.close()
+fout2.close()
+fout3.close()
