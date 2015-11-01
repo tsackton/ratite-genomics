e94eb6a2263173bc710c165f23903c599aabbafe
Tim Sackton <tsackton@oeb.harvard.edu>
2015-11-01 16:25:26 -0500

    Updating phast code

--- alignment/phast/est_rho.sh
+++ alignment/phast/est_rho.sh
@@ -5,17 +5,16 @@
 #SBATCH -p serial_requeue
 #SBATCH -n 1
 #SBATCH -N 1
-#SBATCH --array=0-101
 
 mkdir -p logs
 mkdir -p mods
 INDEX=$(printf "%03d" ${SLURM_ARRAY_TASK_ID})
-for FILE in $(cat part.$INDEX)
+for FILE in $(cat sample.$INDEX)
 do
         SAMP=${FILE%.ss}
         STATUS=$(tail -n 1 ./logs/$SAMP.out)
         if [[ "$STATUS" != "Done." ]]
         then
-		phastCons --expected-length=45 --target-coverage=0.3 --estimate-rho ./mods/$SAMP.rho --no-post-probs --msa-format SS --log ./logs/$SAMP.log ../chunks/$SAMP.ss ../../../neutMods/neut_ver1_final.named.mod &> ./logs/$SAMP.out
+		phastCons --expected-length=$ESTLEN --target-coverage=$TARGETCOV --estimate-rho ./mods/$SAMP.rho --no-post-probs --msa-format SS --log ./logs/$SAMP.log ../chunks/$SAMP.ss ../../neutMods/neut_ver1_final.named.mod &> ./logs/$SAMP.out
 	fi
 done
