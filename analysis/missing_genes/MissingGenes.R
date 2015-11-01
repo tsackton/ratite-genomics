e48f099faf102c20e510847a8c374ae9956e31ca
phil-grayson <pgrayson@fas.harvard.edu>
2015-11-01 16:36:49 -0500

    playing around with r code in MissingGenes.R

--- analysis/missing_genes/MissingGenes.R
+++ analysis/missing_genes/MissingGenes.R
@@ -1,16 +1,55 @@
-setwd("~/Desktop/")
-rA <- read.delim("rheAme_orfs.txt",row.names = 1)
-cP <- read.delim("croPor_orfs.txt",row.names = 1)
-nP <- read.delim("notPer_orfs.txt",row.names = 1)
-fP <- read.delim("falPer_orfs.txt",row.names = 1)
-mU <- read.delim("mesUni_orfs.txt",row.names = 1)
+setwd("~/Desktop/orf_missing/")
+listy <- list.files(path = ".")
+
+rA <- read.delim("rheAme_orfs.txt")
+cP <- read.delim("croPor_orfs.txt")
+nP <- read.delim("notPer_orfs.txt")
+fP <- read.delim("falPer_orfs.txt")
+mU <- read.delim("mesUni_orfs.txt")
 
 all.equal(rA$ChickenSeqID,cP$ChickenSeqID)
 all.equal(rA$ChickenSeqID,nP$ChickenSeqID)
 all.equal(rA$ChickenSeqID,fP$ChickenSeqID)
 all.equal(rA$ChickenSeqID,mU$ChickenSeqID)
 
-All_things <- merge(rA,cP, by.x=row.names,by.y=row.names)
-?merge
-All_things <- rA
-All_things$croPor <- cP$croPor
+path = "~/Desktop/orf_missing/"
+out.file<-""
+file.names <- dir(path, pattern =".txt")
+for(i in 1:length(file.names)){
+  file <- read.delim(file.names[i])
+  out.file <- Reduce(function(x, y) merge(x, y, all=TRUE), list(out.file,file))
+}
+write.table(out.file, file = "testing.txt",sep="\t")
+
+out.file[grep("100170842,Genbank:NP_001124216.1",out.file$ChickenSeqID),]
+rA[grep("100170842,Genbank:NP_001124216.1",rA$ChickenSeqID),]
+
+
+
+Reduce(function(x, y) merge(x, y, all=TRUE), list(rA,cP,nP,fP,mU))
+
+listy <-list.files(path=".")
+LL <- list()
+for(file in listy)
+  LL <- c(LL,c=strsplit(file,"_")[[1]][1])
+
+for(name in LL)
+  this <- name
+  this <- read.delim(paste(name, "_orfs.txt", sep=""))
+
+strsplit(list.files(path=".")[1],"_")[[1]][1]
+
+ldf <- list() # creates a list
+rA <- read.delim("rheAme_orfs.txt")
+listcsv <- dir(pattern = "*.txt") # creates the list of all the csv files in the directory
+for (k in 1:length(listcsv)){
+  ldf[[k]] <- read.delim(listcsv[k])
+}
+str(ldf[[1]]) 
+
+
+
+#All_things <- rA
+#All_things$croPor <- cP$croPor
+
+
