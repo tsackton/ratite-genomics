46a9addac49942213a6bc4e1a670e875cb93893e
phil-grayson <pgrayson@fas.harvard.edu>
2015-11-06 15:45:40 -0500

    added a new functionality to stop_number_3_frame.py.  Now it will give the best frame translation for every transcript into its own file.  Also adding a new version of recordgrab called transcript grab that takes the entire ######,Genbank:XP_##### in sys.stdin[1] and returns only one transcript for each gene instead of all of them as recordgrab does.

---
+++ analysis/missing_genes/transcriptgrab.py
@@ -0,0 +1,14 @@
+from Bio import SeqIO
+import re
+import sys
+import os
+
+fout = open('/n/home12/pgrayson/regal/PseudoSearch/genomes/raw/concat/mfr/'+str(sys.argv[1])+".fa",'w')
+for filename in os.listdir('/n/home12/pgrayson/regal/PseudoSearch/genomes/raw/concat'):
+    if filename.endswith('.fa'):
+        for record in SeqIO.parse(filename,"fasta"):
+            recordgrab = re.search('ChickenGeneID\:([0-9]*.*Genbank\:[A-Za-z0-9/./_]*)',record.description)
+            if recordgrab.group(1) == str(sys.argv[1]):
+                trans = record.seq
+                fout.write(">"+filename.split("_")[1].split(".")[0]+"_"+str(record.description)+"\n"+str(trans)+"\n")
+fout.close()
