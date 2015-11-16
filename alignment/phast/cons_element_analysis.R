0ca380022ed7acaede72772b079c3fc06f584b34
Tim Sackton <tsackton@oeb.harvard.edu>
2015-11-16 12:32:06 -0500

    Updating GFF parsing code.

---
+++ alignment/phast/cons_element_analysis.R
@@ -0,0 +1,4 @@
+#Analyze conserved elements
+
+ver1<-read.table("/Volumes/LaCie/Projects/Current/ratities/phast/final_cons_ver1.bed", header=F)
+ver2<-read.table("/Volumes/LaCie/Projects/Current/ratities/phast/final_cons_ver2.bed", header=F)
