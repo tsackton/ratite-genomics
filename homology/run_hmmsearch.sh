ae62da2275b339dad7a38fe853af55a508864595
Tim Sackton <tsackton@oeb.harvard.edu>
2015-11-18 22:54:18 -0500

    Fixed completion checking in hmmsearch sbatch scripts.

--- homology/run_hmmsearch.sh
+++ homology/run_hmmsearch.sh
@@ -6,14 +6,17 @@
 #SBATCH -n 8
 #SBATCH -N 1
 #SBATCH --array=0-111
+#SBATCH -J hmmsearch
 
 INDEX=$(printf "%03d" ${SLURM_ARRAY_TASK_ID})
 for SAMP in $(cat hmmpart.$INDEX)
 do	
-	if [ ! -e logs/$SAMP.done ]
+	if [ ! -e output/$SAMP.out ]
 	then
-		#not done
-		hmmsearch -E 1e-10 --cpu 8 -Z 19153963251 --tblout output/$SAMP.out --noali hmms/$SAMP.hmm seqs_for_hmm.fa > /dev/null	
-		touch logs/$SAMP.done
+		STATUS=$(tail -n 1 output/$SAMP.out)
+		if [[ $STATUS == *"ok"* ]]
+		then
+			hmmsearch -E 1e-10 --cpu 8 -Z 19153963251 --tblout output/$SAMP.out --noali hmms/$SAMP.hmm seqs_for_hmm.fa > /dev/null	
+		fi
 	fi
 done
