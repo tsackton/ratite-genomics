2d0882eddecfed70fb6722dba6e1f0e1d7553df5
Tim Sackton <tsackton@oeb.harvard.edu>
2015-11-19 11:38:23 -0500

    Adding option parsing to GFF_CDS_parser.py

--- alignment/phast/cons_element_analysis.R
+++ alignment/phast/cons_element_analysis.R
@@ -3,3 +3,24 @@
 ce1<-read.table("~/Projects/birds/ratite_compgen/ratite-genomics/alignment/phast/final_beds/most_conserved_final.tree1.bed", header=F)
 ce2<-read.table("~/Projects/birds/ratite_compgen/ratite-genomics/alignment/phast/final_beds/most_conserved_final.tree2.bed", header=F)
 lowe<-read.table("~/Projects/birds/ratite_compgen/ratite-genomics/alignment/phast/final_beds/lowe_cnees.bed", header=F)
+
+#add lengths to each
+ce1$length<-ce1$V3-ce1$V2
+ce2$length<-ce2$V3-ce2$V2
+lowe$length<-lowe$V3-lowe$V2
+
+#plot length distributions
+plot(density(ce2$length[ce2$length<1000]), xlim=c(0,300), col="red", lwd=2, main="Conserved element length distribution", xlab="Length", bty="n")
+lines(density(lowe$length[lowe$length<1000]), lwd=2, lty="dashed")
+legend("topright", legend=c("Sackton et al", "Lowe et al"), lty=c("solid", "dashed"), col=c("red", "black"), lwd=2, bty="n")
+abline(v=median(ce2$length), col="red", lty="dotted", lwd=2)
+abline(v=median(lowe$length), col="black", lty="dotted", lwd=2)
+
+#length distibutions of elements > 50 bp
+plot(density(ce2$length[ce2$length<1000 & ce2$length > 75]), xlim=c(0,300), col="red", lwd=2, main="Conserved element length distribution (75bp cutoff)", xlab="Length", bty="n")
+lines(density(lowe$length[lowe$length<1000 & lowe$length > 75]), lwd=2, lty="dashed")
+legend("topright", legend=c("Sackton et al", "Lowe et al"), lty=c("solid", "dashed"), col=c("red", "black"), lwd=2, bty="n")
+abline(v=median(ce2$length[ce2$length<1000 & ce2$length > 75]), col="red", lty="dotted", lwd=2)
+abline(v=median(lowe$length[lowe$length<1000 & lowe$length > 75]), col="black", lty="dotted", lwd=2)
+
+plot(hexbin(ce2$length, ce2$V5))
--- analysis/missing_genes/GFF_CDS_parser.py
+++ analysis/missing_genes/GFF_CDS_parser.py
@@ -8,14 +8,28 @@ This file reads through an NCBI gff and creates a new gff with only the lines th
 import csv
 import sys
 import os
+import argparse
 
-phile = str(sys.argv[1])
-fout = open(os.getcwd()+"/out/"+phile.split('.')[0]+".gff", 'w')
+#better option processing
+parser = argparse.ArgumentParser(description="Read a GFF file and export lines with a specified type.")
+parser.add_argument('gff', help='GFF file to process', action='store')
+parser.add_argument('-o', '--out', help='File to write to', action='store')
+parser.add_argument('-t', '--type', help='GFF line type to get (default=CDS); for multiple fields use a comma-delimited list', default='CDS', action='store')
+args=parser.parse_args()
 
-with open(os.getcwd()+"/"+phile, 'rU') as handle: #opens gff in universal mode
+matchTypes = args.type.split(",") #make a list of match types
+
+#outfile
+try:
+	outputFile = args.out
+except:
+	outputFile = args.gff +"/out/" + args.gff.split('.')[0]+ ".gff"
+	
+fout = open(outputFile, 'w')
+with open(args.gff, 'r') as handle: #opens gff in read mode mode
     reader=csv.reader(handle,delimiter='\t') #reads file with tabs as delimiters
     for strLine in reader:
         if len(strLine)==9: #only the lines in proper gff format (length of 9) should be examined
-            if strLine[2]=="CDS": #if a given line in file has CDS in column 3 ...
+            if strLine[2] in matchTypes: #if a given line in file matches types in 3 ...
                 fout.write('\t'.join(strLine) + '\n') #and write that string out
 fout.close()
