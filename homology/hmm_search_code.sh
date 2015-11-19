6babc303a70ab17fbb910083310a7e9945871330
Tim Sackton <tsackton@oeb.harvard.edu>
2015-11-18 22:43:34 -0500

    Initial commitof hmmsearch sbatch script

--- homology/hmm_search_code.sh
+++ homology/hmm_search_code.sh
@@ -29,3 +29,20 @@ cat ../OutputFolder/HOGFasta/*.fa > assigned_prot_seq.fa
 cat *_prot_seqs.fa > seqs_for_hmm.fa
 perl -p -i -e 's/>\w+\|\S*\|(\S+)\s+.*$/>$1/' seqs_for_hmm.fa
 perl -p -i -e 's/(>\w+)\.\d+.*$/$1/' seqs_for_hmm.fa
+
+#prep hmm database
+cat ../hmms/*.hmm > hog_hmms
+
+#should now have a directory with the input file, seqs_for_hmm.fa and the database to search, hog_hmms
+#note that one test profile with hmmsearch takes about 15 seconds
+#there are 27873 profiles to search against 687187 sequences
+#split into 100 array jobs with ~278 profiles each
+#each array gets 8 cpus
+#use a command line this:
+#hmmsearch -E <eval> --cpu 8 --tblout HOG<num>.tabout --noali ../hmms/<INPUT>.hmm seqs_for_hmm.fa 
+
+#first prep file parts
+ls hmms > hmm.list
+perl -p -i -e 's/\.hmm//' hmm.list 
+split -a 3 -d -l 250 hmm.list hmmpart. #make file parts
+
