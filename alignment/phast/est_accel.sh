422b36e8fb8cb456dd6313d1d5730605c62484e1
Tim Sackton <tsackton@oeb.harvard.edu>
2015-12-22 11:03:30 -0500

    Made changes to ratite acceleration test code and added a bunch of species-specific acceleration tests.

--- alignment/phast/est_accel.sh
+++ alignment/phast/est_accel.sh
@@ -1,18 +1,25 @@
 #!/bin/bash
 
-#SBATCH -t 0-08:00
-#SBATCH --mem 1500
+#SBATCH -t 1-00:00
+#SBATCH --mem 400
 #SBATCH -p serial_requeue
 #SBATCH -n 1
 #SBATCH -N 1
-#SBATCH --array=0-101
+#SBATCH -J phyloP
 
-mkdir -p ratite
-mkdir -p tinamou
-INDEX=$(printf "%03d" ${SLURM_ARRAY_TASK_ID})
-for FILE in $(cat part.$INDEX)
-do
-	SAMP=${FILE%.ss}
-	phyloP --method LRT --features LoweCNEEs.galGal4.bed.fixed --mode CONACC --branch rheAme,rhePen,rheAme-rhePen,strCam,casCas,droNov,casCas-droNov,aptHaa-casCas,aptHaa-aptOwe,aptRow,aptHaa,aptOwe,aptHaa-aptRow $HOME/ratite_scratch/phast/neutMods/neut_ver1_final.named.mod $HOME/ratite_scratch/phast/phastCons/galGal/chunks/$SAMP.ss > ./ratite/$SAMP.features.ratite.out
-	phyloP --method LRT --features LoweCNEEs.galGal4.bed.fixed --mode CONACC --branch cryCin,tinGut,cryCin-tinGut,eudEle,notPer,eudEle-notPer,cryCin-eudEle $HOME/ratite_scratch/phast/neutMods/neut_ver1_final.named.mod $HOME/ratite_scratch/phast/phastCons/galGal/chunks/$SAMP.ss > ./tinamou/$SAMP.features.tinamou.out
-done
+NEUTMOD=$1 
+CHR=$2
+ALIGN=$3
+phyloP --method LRT --features $CHR.temp.bed --mode ACC --branch rheAme,rhePen,rheAme-rhePen,strCam,casCas,droNov,casCas-droNov,aptHaa-casCas,aptHaa-aptOwe,aptRow,aptHaa,aptOwe,aptHaa-aptRow $NEUTMOD $ALIGN > ratite/$CHR.Ratite.out
+phyloP --method LRT --features $CHR.temp.bed --mode ACC --branch rheAme $NEUTMOD $ALIGN > tips/$CHR.rheAme.out
+phyloP --method LRT --features $CHR.temp.bed --mode ACC --branch rhePen $NEUTMOD $ALIGN > tips/$CHR.rhePen.out
+phyloP --method LRT --features $CHR.temp.bed --mode ACC --branch strCam $NEUTMOD $ALIGN > tips/$CHR.strCam.out
+phyloP --method LRT --features $CHR.temp.bed --mode ACC --branch casCas $NEUTMOD $ALIGN > tips/$CHR.casCas.out
+phyloP --method LRT --features $CHR.temp.bed --mode ACC --branch droNov $NEUTMOD $ALIGN > tips/$CHR.droNov.out
+phyloP --method LRT --features $CHR.temp.bed --mode ACC --branch aptRow $NEUTMOD $ALIGN > tips/$CHR.aptRow.out
+phyloP --method LRT --features $CHR.temp.bed --mode ACC --branch aptHaa $NEUTMOD $ALIGN > tips/$CHR.aptHaa.out
+phyloP --method LRT --features $CHR.temp.bed --mode ACC --branch aptOwe $NEUTMOD $ALIGN > tips/$CHR.aptOwe.out
+phyloP --method LRT --features $CHR.temp.bed --mode ACC --branch rheAme,rhePen,rheAme-rhePen $NEUTMOD $ALIGN > clades/$CHR.Rhea.out
+phyloP --method LRT --features $CHR.temp.bed --mode ACC --branch casCas,droNov,casCas-droNov $NEUTMOD $ALIGN > clades/$CHR.Casuar.out
+phyloP --method LRT --features $CHR.temp.bed --mode ACC --branch aptHaa-casCas,aptHaa-aptOwe,aptRow,aptHaa,aptOwe,aptHaa-aptRow $NEUTMOD $ALIGN > clades/$CHR.Kiwi.out
+phyloP --method LRT --features $CHR.temp.bed --mode ACC --branch cryCin,tinGut,cryCin-tinGut,eudEle,notPer,eudEle-notPer,cryCin-eudEle $NEUTMOD $ALIGN > tinamou/$CHR.Tinamou.out		
