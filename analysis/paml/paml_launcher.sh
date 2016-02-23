8ec51bfee48aaf8a33aee585485494297162c4fc
Tim Sackton <tsackton@oeb.harvard.edu>
2016-02-23 16:17:20 -0500

    More updates to PAML launcher code.

--- analysis/paml/paml_launcher.sh
+++ analysis/paml/paml_launcher.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-#SBATCH -p tsackton,serial_requeue
+#SBATCH -p serial_requeue
 #SBATCH -n 1
 #SBATCH -N 1
-#SBATCH --mem 1000
-#SBATCH -t 0-18:00
+#SBATCH --mem 3000
+#SBATCH -t 1-00:00
 #SBATCH -o ./logs/paml_%A_%a.out
 #SBATCH -e ./logs/paml_%A_%a.err
 
-HOGLIST=($(cat rerun.round2))
+HOGLIST=($(cat $1))
 HOGRUN=${HOGLIST[$SLURM_ARRAY_TASK_ID]}
 ./run_paml.sh $HOGRUN
--- analysis/paml/submit_paml_jobs.sh
+++ analysis/paml/submit_paml_jobs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export HOGLIST=($(cat rerun.round2))
+export HOGLIST=($(cat $1))
 NUMHOG=${#HOGLIST[@]}
 ZBNUMHOG=$(($NUMHOG - 1))
-sbatch --array=0-$ZBNUMHOG ./paml_launcher.sh
+sbatch --array=0-$ZBNUMHOG ./paml_launcher.sh $1
