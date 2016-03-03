d31bab29667ca566c10026044f8c114fba96877f
Tim Sackton <tsackton@oeb.harvard.edu>
2016-03-03 14:31:04 -0500

    Moa processing code.

--- alignment/cnee_presence/ratiteCheck.sh
+++
@@ -1,4 +0,0 @@
-for SP in strCam droNov casCas aptHaa aptRow aptOwe rheAme rhePen
-do
-	halLiftover ~/ratite_scratch/wga/ratite_final_20150627/ratiteAlign.hal galGal final_ces.tree2.bed $SP $SP.bed &
-done
