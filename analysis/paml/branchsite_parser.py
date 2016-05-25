162b3e1efdd7d8f98260d2cc27b5a4955182b172
Tim Sackton <tsackton@oeb.harvard.edu>
2016-05-25 12:57:21 -0400

    Updates to branch-site parser.

--- analysis/paml/branchsite_parser.py
+++ analysis/paml/branchsite_parser.py
@@ -112,7 +112,7 @@ def parse_hogs(hoglist,model):
             try:
                 species_tree = Phylo.read(sptreepath, "newick")
             except:
-                print("Couldn't get species tree for ", hog)
+#                print("Couldn't get species tree for ", hog)
                 continue
                     
             cml = codeml.Codeml()
@@ -123,12 +123,16 @@ def parse_hogs(hoglist,model):
                 parsed_trees = parse_trees(tree_file,species_tree)
                 parsed_results = parse_multitree_results(results_file)
                 #check that we have a result for each tree
-                if len(parsed_trees) != len(parsed_results):
-                    print("Warning, number of trees is different from number of results for " + hog + " in " + results_file)
+                if len(parsed_trees) < len(parsed_results):
+                    print("Warning, too few trees for number of results for " + hog + " in " + results_file)
                     continue
+                elif len(parsed_trees) > len(parsed_results):
+                    #remove trees that aren't in results
+                    trimmed_trees = {x:parsed_trees[x] for x in parsed_results.keys()}
+                    parsed_trees = trimmed_trees
                        
             except:
-                print("Couldn't parse paml for ", hog)
+#                print("Couldn't parse paml for ", hog)
                 continue
                 
             if hog in final_results:
@@ -177,11 +181,12 @@ def print_results (results, handle, model):
 
 def main():
 
-    hogfile_toparse = sys.argv[1]    
+    hogfile_toparse = sys.argv[1]   
+    resfile_foroutput = sys.argv[2] 
     #get hog list, for now from a file
     with open(hogfile_toparse) as hfile:
         hogs=[line.rstrip('\n') for line in hfile]
-    with open("parsed_paml_results.txt", 'w') as ofile:
+    with open(resfile_foroutput, 'w') as ofile:
         #print header
         print("hog", "model", "treenum", "foreground_species", "species_tree", "newick_string", "lnl", "treelen", "class0_prop", "class0_fore", "class0_back", "class1_prop", "class1_fore", "class1_back", "class2a_prop", "class2a_fore", "class2a_back", "class2b_prop", "class2b_fore", "class2b_back", sep="\t", end="\n", file=ofile)
 
