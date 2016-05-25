c87182468db044da0126b1db2f41181af167e0a6
Tim Sackton <tsackton@oeb.harvard.edu>
2016-05-25 13:44:18 -0400

    more small edits to parser.

--- analysis/paml/branchsite_parser.py
+++ analysis/paml/branchsite_parser.py
@@ -190,6 +190,7 @@ def main():
     #get hog list, for now from a file
     with open(hogfile_toparse) as hfile:
         hogs=[line.rstrip('\n') for line in hfile]
+                
     with open(resfile_foroutput, 'w') as ofile:
         print("hog", "model", "treenum", "foreground_species", "species_tree", "newick_string", "lnl", "treelen", "class0_prop", "class0_fore", "class0_back", "class1_prop", "class1_fore", "class1_back", "class2a_prop", "class2a_fore", "class2a_back", "class2b_prop", "class2b_fore", "class2b_back", sep="\t", end="\n", file=ofile)
 
