5fed37a075f54341075845a8b4eab0956a78f6aa
Tim Sackton <tsackton@oeb.harvard.edu>
2016-05-27 20:44:13 -0400

    Initial commit of working version of site model parser.

--- analysis/paml/branchsite_parser.py
+++ analysis/paml/branchsite_parser.py
@@ -102,7 +102,32 @@ def parse_multitree_results (file):
         paml_results[tree_index] = parsed
     return(paml_results)
 
-def parse_hogs(hoglist,model,verbose):
+def parse_multitree_multimodel_results (file):
+     #takes a paml output file, splits on TREE #, returns list of output files to parse
+     
+    with open(file, 'r') as pf:
+        results=pf.read()
+    
+    #split results
+    models = results.split("\nModel ")
+    header = models.pop(0)
+    paml_strings = {}
+    paml_results = {}
+    for model in models:
+        pieces = model.split("\nTREE")
+        subheader = "\nModel " + pieces.pop(0)
+        for i in range(0,len(pieces)):
+            tree_index = i+1
+            try:
+                paml_strings[tree_index] += subheader + "\nTREE" + pieces[i]
+            except KeyError:
+                paml_strings[tree_index] = header + subheader + "\nTREE" + pieces[i]
+    for tree_num in paml_strings:
+        paml_results[tree_num] = parse_codeml_string(io.StringIO(paml_strings[tree_num]))
+
+    return(paml_results)
+
+def parse_hogs(hoglist,model,verbose,multisite):
     #take list of hogs, return parsed final results dictionary
     final_results = {}
     for hog in hoglist:
@@ -129,7 +154,10 @@ def parse_hogs(hoglist,model,verbose):
                 tree_file = pamldir + "/" + fullpath + "/" + cml.tree
                 #now process
                 parsed_trees = parse_trees(tree_file,species_tree)
-                parsed_results = parse_multitree_results(results_file)
+                if multisite:
+                    parsed_results = parse_multitree_multimodel_results(results_file)
+                else:
+                    parsed_results = parse_multitree_results(results_file)
                 #check that we have a result for each tree
                 if len(parsed_trees) < len(parsed_results):
                     print("Warning, too few trees for number of results for " + hog + " in " + results_file)
@@ -195,6 +223,7 @@ def main():
     hogfile_toparse = sys.argv[1]   
     resfile_foroutput = sys.argv[2] 
     verbose=1
+    multisite=False
     #get hog list, for now from a file
     with open(hogfile_toparse) as hfile:
         hogs=[line.rstrip('\n') for line in hfile]
@@ -204,7 +233,7 @@ def main():
         print("hog", "model", "treenum", "foreground_species", "species_tree", "newick_string", "lnl", "treelen", "class0_prop", "class0_fore", "class0_back", "class1_prop", "class1_fore", "class1_back", "class2a_prop", "class2a_fore", "class2a_back", "class2b_prop", "class2b_fore", "class2b_back", sep="\t", end="\n", file=ofile, flush=True)
 
         for model in ("branchsite", "branchsitenull"):
-            results = parse_hogs(hogs,model,verbose)
+            results = parse_hogs(hogs,model,verbose,multisite)
             #print out
             print_results(results, ofile, model)
                  
---
+++ analysis/paml/site_parser.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python
+
+import os
+import sys
+from Bio import Phylo
+from Bio.Phylo.PAML import codeml
+from Bio.Phylo.PAML import _parse_codeml
+from Bio.Phylo.Consensus import _BitString
+import io
+import re
+
+#should refactor this into an actual module I guess
+
+from branchsite_parser import parse_hogs
+
+def print_results(results, handle, model):
+    for hog in results.keys():
+        tree_len = len(results[hog]['trees'])
+        res_len = len(results[hog]['results'])
+        for i in range(1,res_len+1):
+            trees = results[hog]['trees'].get(i, {})
+            res = results[hog]['results'].get(i, {})
+            #parse results
+            for modelnum in sorted(res.get('NSsites',{}).keys(), key=int):
+                #fixed options
+                res_lnl = res.get('NSsites', {}).get(modelnum, {}).get('lnL')
+                res_treelen = res.get('NSsites', {}).get(modelnum, {}).get('tree length')
+                res_kappa = res.get('NSsites', {}).get(modelnum, {}).get('parameters', {}).get('kappa')
+                #what to get depends on the model number
+                if modelnum == 0:
+                    #m0 case
+                    res_omega = res.get('NSsites', {}).get(modelnum, {}).get('parameters', {}).get('omega')
+                elif modelnum == 1 or modelnum == 2:
+                    #m1/m2 case, use site classes 
+                    res_omega = None
+                    res_omegas = res.get('NSsites', {}).get(modelnum, {}).get('parameters', {}).get('site classes', {})
+                    for sc in sorted(res_omegas.keys(), key=int):
+                        if res_omega is None:
+                            res_omega = str(round(res_omegas[sc]['proportion'],3)) + ":" + res_omegas[sc]['omega']
+                        else:
+                            res_omega += "," + str(round(res_omegas[sc]['proportion'],3)) + ":" + res_omegas[sc]['omega']
+                elif modelnum == 7:
+                    #m7 case
+                    m7_p = str(round(float(res.get('NSsites', {}).get(modelnum, {}).get('parameters', {}).get('p')),3))
+                    m7_q = str(round(float(res.get('NSsites', {}).get(modelnum, {}).get('parameters', {}).get('q')),3))
+                    res_omega = m7_p + "/" + m7_q
+                elif modelnum == 8:
+                    m8_p = str(round(float(res.get('NSsites', {}).get(modelnum, {}).get('parameters', {}).get('p')),3))
+                    m8_q = str(round(float(res.get('NSsites', {}).get(modelnum, {}).get('parameters', {}).get('q')),3))
+                    m8_p0 = str(round(float(res.get('NSsites', {}).get(modelnum, {}).get('parameters', {}).get('p0')),3))
+                    m8_p1 = str(round(1 - float(m8_p0),3))
+                    m8_w = str(round(float(res.get('NSsites', {}).get(modelnum, {}).get('parameters', {}).get('w')),3))
+                    res_omega = m8_p0 + ":" + m8_p + "/" + m8_q + "," + m8_p1 + ":" + m8_w
+                else:
+                    pass
+                    
+                print(hog, model, i, trees.get('is_species_tree'), trees.get('original'), modelnum, res_lnl, res_treelen, res_kappa, res_omega, sep="\t", end="\n", file=handle)
+ 
+
+def main():
+    print("Starting to parse.")
+    hogfile_toparse = sys.argv[1]
+    resfile_foroutput = sys.argv[2]
+    verbose=0
+    with open(hogfile_toparse) as hfile:
+        hogs=[line.rstrip("\n") for line in hfile]
+    
+    print("Done getting files.")
+    with open(resfile_foroutput, 'w') as ofile:
+        print("hog", "model", "treenum", "species_tree", "newick_string", "model_num", "lnl", "treelen", "kappa", "omega", sep="\t", end="\n", file=ofile, flush=True)
+        results = parse_hogs(hogs,"site",verbose,True)
+        print_results(results, ofile, "site")
+
+if __name__ == "__main__":
+    main()
