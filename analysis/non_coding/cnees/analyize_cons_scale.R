36764fe33ac31475da0be12ed55f8fbc4733549a
Tim Sackton <tsackton@oeb.harvard.edu>
2017-01-25 16:05:56 -0500

    Bayesian ratite accelerated region code updates.

--- analysis/non_coding/cnees/analyize_cons_scale.R
+++ analysis/non_coding/cnees/analyize_cons_scale.R
@@ -9,6 +9,8 @@ ratite<-c("aptRow", "aptOwe", "aptHaa", "casCas", "droNov", "rheAme", "rhePen", 
 nonbird<-c("allMis", "allSin", "croPor", "gavGan", "chrPic", "cheMyd", "anoCar", "taeGut.aptHaa", "allMis.allSin", "croPor.gavGan", "allMis.croPor", "taeGut.allMis", "chrPic.cheMyd", "taeGut.chrPic", "taeGut.anoCar")
 paleo<-c("cryCin", "tinGut", "eudEle", "notPer", "aptHaa.casCas", "aptHaa.rheAme", "cryCin.tinGut", "eudEle.notPer", "cryCin.eudEle", "cryCin.anoDid", "aptHaa.cryCin", "aptHaa.strCam")
 penguin<-c("pygAde", "aptFor", "aptFor.pygAde")
+vocal.tips<-c("taeGut", "ficAlb", "pseHum", "corBra", "melUnd", "calAnn")
+ratite.tips<-c("aptRow", "aptOwe", "aptHaa", "casCas", "droNov", "rheAme", "rhePen", "anoDid", "strCam")
 neognath<-colnames(cons)[!(colnames(cons) %in% nonbird | colnames(cons) %in% ratite | colnames(cons) %in% paleo | colnames(cons) %in% penguin)]
 allbirds<-c(neognath,penguin,ratite,paleo)
 cons$ratite<-apply(cons[,ratite], 1, function(x) sum(x != 1))
@@ -16,111 +18,152 @@ cons$nonbird<-apply(cons[,nonbird], 1, function(x) sum(x != 1))
 cons$penguin<-apply(cons[,penguin], 1, function(x) sum(x != 1))
 cons$paleo<-apply(cons[,paleo], 1, function(x) sum(x != 1))
 cons$neognath<-apply(cons[,neognath], 1, function(x) sum(x != 1))
-#birdonly<-subset(cons, cons$taeGut.aptHaa == 1)
 
-#random<-c("taeGut","nipNip", "falPer", "colLiv")
-#random.set<-cons[,random]
-#random.set$ct = apply(random.set, 1, function(x) sum(x != 1))
-#ratite.sub<-c("aptRow", "casCas", "rheAme", "strCam")
-#ratite.set<-cons[,ratite.sub]
-#ratite.set$ct = apply(ratite.set, 1, function(x) sum(x != 1))
+#for simplicity, let's analyze just tips
+tips<-cons[,c(1:36)]
 
-#highcons<-subset(birdonly, paleo==0 & neognath < 2)
-#highcons2<-subset(birdonly, paleo+ratite < 2)
+#proof of principle
+#take a random sample of neognaths
+random_set=data.frame(ct0=numeric(), ct1=numeric(), ct2=numeric(), ct3=numeric(), ct4=numeric(), ct5=numeric())
+ratite_set=data.frame(ct0=numeric(), ct1=numeric(), ct2=numeric(), ct3=numeric(), ct4=numeric(), ct5=numeric())
 
-#temp
-#cands<-read.table("cnee_permutations/ratite_candidates_final.tsv", header=T, sep="\t")
-#cands<-merge(cands, cons, all.x=T, all.y=F, by.x="name", by.y="row.names")
-#cands2<-subset(cands, neognath < 2 & paleo==0)
+for (i in 1:100) {
+  target<-sample(neognath[!grepl(".", neognath, fixed=T)])[1:5]
+  res<-data.frame(nontarget=apply(tips[,!(colnames(tips) %in% target)], 1, function(x) sum(x != 1)), target=factor(apply(tips[,target], 1, function(x) sum (x != 1)), levels=c(0,1,2,3,4,5)))
+  random_set[i,]=table(res$target[res$nontarget==0])[1:6]
+  target<-sample(ratite[!grepl(".", ratite, fixed=T)])[1:5]
+  res<-data.frame(nontarget=apply(tips[,!(colnames(tips) %in% target)], 1, function(x) sum(x != 1)), target=factor(apply(tips[,target], 1, function(x) sum (x != 1)), levels=c(0,1,2,3,4,5)))
+  ratite_set[i,]=table(res$target[res$nontarget==0])[1:6]
+}
 
-#barplot(table(highcons2$neognath)/length(highcons$neognath), names=c(round(seq(0,1,by=1/42),2)), las=2)
+tips$total.loss = apply(tips, 1, function(x) sum(x != 1))
+tips$ratite.loss = apply(tips[,ratite[!grepl(".", ratite, fixed=T)]], 1, function(x) sum(x != 1))
+tips$vl.loss = apply(tips[,c("taeGut", "ficAlb", "pseHum", "corBra", "melUnd", "calAnn")], 1, function(x) sum (x != 1))
 
-#barplot(table(highcons$ratite)/length(highcons$ratite), las=1, xlab="# of ratite lineages where Z != conserved")
-#barplot(table(cands$ratite)/length(cands$ratite), las=1, xlab="# of ratite lineages where Z != conserved")
+tips[tips$total.loss==9 & tips$ratite.loss == 9,]
 
+#three species test:
+all_ratite_3sp=list()
+for (rhea in c("rheAme", "rhePen")) {
+  for (kiwi in c("aptHaa", "aptRow", "aptOwe")) {
+    for (cas in c("droNov", "casCas")) {
+      ratite_set = c(rhea, kiwi, cas, "strCam", "anoDid")
+      ratite.3.set = combn(ratite_set, 3, simplif=F)
+      all_ratite_3sp = c(all_ratite_3sp, ratite.3.set)
+    }
+  }
+}
+all_ratite_3sp = unique(all_ratite_3sp)
 
-cons$ratite.test<-apply(cons[,c("aptRow", "casCas", "rheAme", "strCam","anoDid")], 1, function(x) sum(x != 1))
-cons$ratite.3.test<-apply(cons[,c("aptRow","strCam","anoDid")], 1, function(x) sum(x != 1))
-cons$ratite.control<-apply(cons[,allbirds[!allbirds %in% ratite]], 1, function(x) sum(x != 1))
-cons$random1.test<-apply(cons[,c("colLiv", "chaVoc", "halLeu", "taeGut", "nipNip")], 1, function(x) sum(x != 1))
-cons$random1.control<-apply(cons[,allbirds[!allbirds %in% c("colLiv", "chaVoc", "halLeu", "taeGut", "nipNip")]], 1, function(x) sum(x != 1))
-cons$vl.test<-apply(cons[,c("calAnn", "melUnd", "ficAlb")], 1, function(x) sum(x != 1))
-cons$vl.control<-apply(cons[,allbirds[!allbirds %in% c('calAnn', 'corBra', 'serCan', 'geoFor', 'melUnd', 'pseHum', 'taeGut', 'ficAlb')]], 1, function(x) sum(x != 1))
-cons$random2.test<-apply(cons[,c("falPer", "picPub", "lepDis")], 1, function(x) sum(x != 1))
-cons$random2.control<-apply(cons[,allbirds[!allbirds %in% c("falPer", "picPub", "lepDis")]], 1, function(x) sum(x != 1))
-
-barplot(t(matrix(c(table(cons$ratite.test[cons$ratite.control==0 & cons$ratite.test>1]), c(table(cons$random1.test[cons$random1.control==0 & cons$random1.test>1]),0,0)),ncol=2)),beside=T,col=c("red", "blue"),names=c("2", "3", "4", "5"), ylab="# of Elements", xlab="# of Lineages", main="Elements Uniquely Accelerated in Target Lineages")
-legend("topright", legend=c("Ratite", "Random Control"), col=c("red", "blue"), pch=15, bty="n")
-
-
-barplot(t(matrix(c(table(cons$vl.test[cons$vl.control==0 & cons$vl.test>1]), c(table(cons$random2.test[cons$random2.control==0 & cons$random2.test>1]),0)),ncol=2)),beside=T,col=c("darkgreen", "blue"),names=c("2", "3"), ylab="# of Elements", xlab="# of Lineages", main="Elements Uniquely Accelerated in Target Lineages")
-legend("topright", legend=c("Vocal Learners", "Random Control"), col=c("darkgreen", "blue"), pch=15, bty="n")
-
-barplot(t(matrix(c(table(cons$ratite.3.test[cons$ratite.control==0 & cons$ratite.3.test>1]), c(table(cons$random2.test[cons$random2.control==0 & cons$random2.test>1]),0)),ncol=2)),beside=T,col=c("red", "blue"),names=c("2", "3"), ylab="# of Elements", xlab="# of Lineages", main="Elements Uniquely Accelerated in Target Lineages")
-legend("topright", legend=c("Ratite", "Random Control"), col=c("red", "blue"), pch=15, bty="n")
-
-barplot(t(matrix(c(table(cons$ratite.3.test[cons$ratite.control==0 & cons$ratite.3.test>1]), table(cons$vl.test[cons$vl.control==0 & cons$vl.test>1]), c(table(cons$random2.test[cons$random2.control==0 & cons$random2.test>1]),0)),ncol=3)),beside=T,col=c("red", "darkgreen", "blue"),names=c("2", "3"), ylab="# of Elements", xlab="# of Lineages", main="Elements Uniquely Accelerated in Target Lineages")
-legend("topright", legend=c("Ratite", "Vocal Learners", "Random Control"), col=c("red", "darkgreen", "blue"), pch=15, bty="n")
+all_ratite_3sp_strict=list()
+for (third_rat in c("rheAme", "rhePen","aptHaa", "aptRow", "aptOwe","droNov", "casCas")) {
+      ratite_set = list(c(third_rat, "strCam", "anoDid"))
+      all_ratite_3sp_strict = c(all_ratite_3sp_strict, ratite_set)
+}
 
 
-barplot(t(matrix(c(table(cons$ratite.3.test[cons$ratite.control<3 & cons$ratite.3.test>1]), table(cons$vl.test[cons$vl.control<3 & cons$vl.test>1]), table(cons$random2.test[cons$random2.control<3 & cons$random2.test>1])),ncol=3)),beside=T,col=c("red", "darkgreen", "blue"),names=c("2", "3"), ylab="# of Elements", xlab="# of Lineages", main="Elements Uniquely Accelerated in Target Lineages")
-legend("topright", legend=c("Ratite", "Vocal Learners", "Random Control"), col=c("red", "darkgreen", "blue"), pch=15, bty="n")
+all_vl_3sp=list()
+for (pass in c("taeGut", "ficAlb", "pseHum", "corBra")) {
+  vl.3.set = list(c(pass, "melUnd", "calAnn"))
+  all_vl_3sp=c(all_vl_3sp, vl.3.set)
+}
 
-barplot(t(matrix(c(table(cons$ratite.test[cons$ratite.control<3 & cons$ratite.test>1]), c(table(cons$random1.test[cons$random1.control<3 & cons$random1.test>1]),0)),ncol=2)),beside=T,col=c("red", "blue"),names=c("2", "3", "4", "5"), ylab="# of Elements", xlab="# of Lineages", main="Elements Uniquely Accelerated in Target Lineages")
-legend("topright", legend=c("Ratite", "Random Control"), col=c("red", "blue"), pch=15, bty="n")
+tips.clean=tips[,c(1:36)]
+tips.clean[tips.clean==1]=5
+tips.clean[tips.clean<5]=1
+tips.clean[tips.clean==5]=0
 
-cons$ratite.conv<-0
-cons$ratite.conv[cons$ratite>2 & cons$ratite.control < 2]<-1
+nonratite.losses = rowSums(tips.clean[,!colnames(tips.clean) %in% ratite.tips])
+nonvl.losses = rowSums(tips.clean[,!colnames(tips.clean) %in% vocal.tips])
+ratite.losses = rowSums(tips.clean[,ratite.tips])
+all.losses<-data.frame(ratite=ratite.losses, vl=rowSums(tips.clean[,vocal.tips]), total=rowSums(tips.clean))
 
-cons$vl.conv<-0
-cons$vl.conv[cons$vl.control < 2 & cons$vl.test>1]<-1
+#candidates
+ratite.cands<-rownames(all.losses[all.losses$ratite == all.losses$total & all.losses$ratite > 4,])
+ratite.cands.loose<-rownames(all.losses[all.losses$ratite == all.losses$total & all.losses$ratite > 1,])
+ratite.cands.strict<-rownames(all.losses[all.losses$ratite == all.losses$total & all.losses$ratite >= 8,])
+  
+#add info
+ce.info<-read.csv("~/Dropbox/Work/EggCNEEs/October 2016/gg.final.annotated.csv")
+names(ce.info)[2]="cnee"
+ce.info$rar = ce.info$cnee %in% ratite.cands
+ce.info$rar.s = ce.info$cnee %in% ratite.cands.strict
+ce.info$rar.l = ce.info$cnee %in% ratite.cands.loose
 
-cons$origin.crude=NA
-cons$origin.crude[cons$taeGut.aptHaa==0]="in.birds"
-cons$origin.crude[cons$taeGut.aptHaa==1]="base.birds"
-cons$origin.crude[cons$taeGut.allMis==1]="base.arch"
-cons$origin.crude[cons$taeGut.chrPic==1]="rept"
+write.table(unique(ce.info$best_ens[ce.info$rar == T]), file="rar.ensID", quote=F, sep="\t", row.names=F, col.names=F)
+write.table(unique(ce.info$best_ens[ce.info$rar.s == T]), file="rar.strict.ensID", quote=F, sep="\t", row.names=F, col.names=F)
+write.table(unique(ce.info$best_ens[ce.info$rar.l == T]), file="rar.loose.ensID", quote=F, sep="\t", row.names=F, col.names=F)
+write.table(unique(ce.info$best_ens), file="rar_background.ensID", quote=F, sep="\t", row.names=F, col.names=F)
+table(ce.info$rar)
 
-prop.table(table(cons$ratite.conv[cons$origin.crude != "in.birds"], cons$origin.crude[cons$origin.crude != "in.birds"]),1)
+#ratite counts
+rat.3conv = numeric()
+for (rat in all_ratite_3sp) {
+  ratct<-data.frame(ratite=rowSums(tips.clean[,rat]),total=nonratite.losses)
+  ratct=subset(ratct, total == 0 & ratite > 0)
+  rat.3conv=c(rat.3conv,sum(ratct$ratite==3))
+}
 
-acc.elements<-rownames(cons[cons$ratite.conv==1,])
-acc.elem.vl<-rownames(cons[cons$vl.conv==1,])
+rat.3conv.strict=numeric()
+for (rat in all_ratite_3sp_strict) {
+  ratct<-data.frame(ratite=rowSums(tips.clean[,rat]),total=nonratite.losses)
+  ratct=subset(ratct, total == 0 & ratite > 0)
+  rat.3conv.strict=c(rat.3conv.strict,sum(ratct$ratite==3))
+}
 
-#compare to previous
-orig.cand<-read.table("../cnee_permutations/ratite_candidates_final.tsv", header=T, sep="\t")
-orig.cand$cons.scale = orig.cand$name %in% acc.elements
-#this doesn't work because I need to read NCBI key in
-#ce.info<-read.csv("~/Dropbox/Work/EggCNEEs/October 2016/gg.final.annotated.csv")
-#ce.info$ratite.conv = ce.info$gg.cnee.id %in% acc.elements
-#ce.info<-merge(ce.info, ncbikey, by.x="best_ens", by.y="ensembl", all.x=T, all.y=F)
-#ce.info$vl.conv = ce.info$gg.cnee.id %in% acc.elem.vl
-#genes<-unique(ce.info$best_ens[ce.info$ratite.conv])
-#background<-unique(ce.info$best_ens)
-#write.table(genes, file="ratite.conv.cnee.genes", quote=F, row.names=F, col.names=F)
-#write.table(ce.info$best_ens[ce.info$vl.conv], file="vl.conv.cnee.genes", quote=F, row.names=F, col.names=F)
+#vl counts
+vl.3conv = numeric()
+for (vl in all_vl_3sp) {
+  ratct<-data.frame(ratite=rowSums(tips.clean[,vl]),total=nonvl.losses)
+  ratct=subset(ratct, total == 0 & ratite > 0)
+  vl.3conv=c(vl.3conv,sum(ratct$ratite==3))
+}
 
-#write.table(background, file="background.cnee.genes", quote=F, row.names=F, col.names=F)
+#10000 random sets
+rand.3conv=numeric()
+for (i in 0:10000) {
+  targets=sample(colnames(tips.clean))[1:3]
+  ratct<-data.frame(ratite=rowSums(tips.clean[,targets]), total=rowSums(tips.clean))
+  ratct=subset(ratct, ratite == total & ratite > 0)
+  rand.3conv[i] = sum(ratct$ratite == 3)
+}
 
-####making final Bayesian file for Phil####
-#add family level ratite data
-rhea<-c("rheAme", "rhePen", "rheAme.rhePen")
-kiwi<-c("aptHaa", "aptRow", "aptOwe", "aptHaa.aptOwe", "aptHaa.aptRow")
-cas<-c("casCas", "droNov", "casCas.droNov")
-moa<-c("anoDid")
-ost<-c("strCam")
 
-cons$rhea<-apply(cons[,rhea], 1, function(x) sum(x != 1)/3)
-cons$kiwi<-apply(cons[,kiwi], 1, function(x) sum(x != 1)/5)
-cons$cas<-apply(cons[,cas], 1, function(x) sum(x != 1)/3)
-cons$ostrich<-apply(cons[,ost, drop=F], 1, function(x) sum(x != 1)/1)
-cons$moa<-apply(cons[,moa, drop=F], 1, function(x) sum(x != 1)/1)
+#save permutation
+write.table(rand.3conv, file="rand.3conv.perms", quote=F, sep="\t", row.names=F, col.names=F)
 
-cons$ratite.conv<-apply(cons[,c("rhea", "kiwi", "cas", "ostrich","moa")], 1, function(x) sum(x == 1))
-cons$ratite.conv[cons$paleo > 0] = 0
+#compare
+plot(y=seq(10000,1,-1)/10000,x=sort(rand.3conv), type="l", col="red", lwd=3, lty="dashed", xlim=c(0,100), las=1, xlab="Number Convergent", ylab="Proportion")
+points(y=0.2, x=mean(rat.3conv), col="blue", pch=16, cex=1.5)
+points(y=0.2, x=mean(rat.3conv.strict), col="blue", pch=17, cex=1.5)
+points(y=0.2, x=mean(vl.3conv), col="darkgreen", pch=16, cex=1.5)
+legend("topright", legend=c("Vocal Learners", "Ratites", "Ratites [strict]"), pch=c(16,16,17), col=c("darkgreen", "blue", "blue"))
+text(x=mean(vl.3conv)-1, y=0.25, labels=paste("P=", sum(rand.3conv >= mean(vl.3conv))/10000), adj=c(0,1))
+text(x=mean(rat.3conv.strict)-1, y=0.25, labels=paste("P=", sum(rand.3conv >= mean(rat.3conv.strict))/10000), adj=c(0.3,1))
+text(x=mean(rat.3conv)-1, y=0.25, labels=paste("P=", sum(rand.3conv >= mean(rat.3conv))/10000), adj=c(0,1))
 
-#subset for Phil
-cons.cand<-subset(cons, select=c("ratite.conv", "kiwi", "cas", "rhea", "ostrich", "moa", "rheAme", "nonbird", "ratite", "penguin", "neognath", "paleo"))
-cons.cand$rheAme[cons.cand$rheAme != "1"] = "neut/missing"
-cons.cand$rheAme[cons.cand$rheAme == "1"] = "conserved"
+table(all.losses$ratite[all.losses$total == all.losses$ratite & all.losses$total > 0])
+ratite.conv.all.cnees<-rownames(all.losses[all.losses$total == all.losses$ratite & all.losses$total > 0,])
+tips.ratite.losses<-tips.clean[ratite.conv.all.cnees,c("anoDid", "strCam", "rheAme", "rhePen", "casCas", "droNov", "aptHaa", "aptRow", "aptOwe")]
+tips.ratite.losses$rhea = pmax(tips.ratite.losses$rheAme,tips.ratite.losses$rhePen)
+tips.ratite.losses$kiwi = pmax(tips.ratite.losses$aptOwe,tips.ratite.losses$aptRow,tips.ratite.losses$aptHaa)
+tips.ratite.losses$cas = pmax(tips.ratite.losses$casCas,tips.ratite.losses$droNov)
+tips.ratite.losses$clades = tips.ratite.losses$anoDid + tips.ratite.losses$strCam +  tips.ratite.losses$rhea + tips.ratite.losses$kiwi +  tips.ratite.losses$cas
+tips.ratite.losses$total = rowSums(tips.ratite.losses[,c(1:9)])
 
-write.table(cons.cand, file="cons_cand_2016Dec06.txt", row.names=T, sep="\t", quote=F, col.names=T)
+#add ce.info
+ratites.conv.losses.final.bayesian<-merge(tips.ratite.losses, ce.info[,c(2,4,5,7,8)], by.x=0, by.y=1)
+write.table(ratites.conv.losses.final.bayesian, file="final_bayesian_conv_Jan2017.txt", quote=F, row.names=F, sep="\t")
+
+#ages - note that ratite convergent seem to be older than all CNEEs
+
+nodekey=data.frame(node=factor(c("vertebrate","node_112","node_108","node_107","node_102","node_99","node_95","node_91","node_79","node_59","node_58"), levels=c("vertebrate","node_112","node_108","node_107","node_102","node_99","node_95","node_91","node_79","node_59","node_58")), name=c("vertebrate", "tetrapods","amniotes","reptiles","turtles","Archosaurs","Birds","Neognaths","Galloanseriformes","Galliformes","Chicken"))
+
+
+ratites.conv.losses.final.bayesian=merge(nodekey,ratites.conv.losses.final.bayesian, by.y="ancestralnode", by.x="node")
+ce.info<-merge(nodekey, ce.info, by.y="ancestralnode", by.x="node")
+
+plot(table(ratites.conv.losses.final.bayesian$node[ratites.conv.losses.final.bayesian$clades>1])/length(ratites.conv.losses.final.bayesian$node[ratites.conv.losses.final.bayesian$clades>1]), type="p", pch=16, col="blue", las=2, ylab="Prop Origination", xaxt="n", ylim=c(0,0.5), xlim=c(1,7))
+points(table(ce.info$node[ce.info$node %in% c("node_102", "node_107", "node_108", "node_112", "node_95", 'node_99', "vertebrate")])/length(ce.info$node[ce.info$node %in% c("node_102", "node_107", "node_108", "node_112", "node_95", 'node_99', "vertebrate")]), type="p", pch=16, col="red")
+axis(side=1, at=c(1,2,3,4,5,6,7), labels=nodekey$name[1:7], las=2)
+legend("topleft", legend=c("Ratite Convergently Accelerated Regions", "All CNEEs"), pch=16, col=c("blue", "red"), bty="n")
