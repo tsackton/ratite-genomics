e61dce431dd0290845c6870bc471b5d29a56aeff
Tim Sackton <tsackton@oeb.harvard.edu>
2017-03-30 10:31:30 -0400

    Updating relax and branch code.

--- analysis/protein_coding/hyphy_relax/clean_relaxed_output.sh
+++ analysis/protein_coding/hyphy_relax/clean_relaxed_output.sh
@@ -1,5 +1,5 @@
-for GROUP in ratite tinamou vl rand
+for GROUP in Ratite Tinamou VL RND
 do
-	perl -p -e 's/\// /g' ${GROUP}_relax_Pval | perl -p -e 's/:/ /' | perl -p -e 's/\[RELAX\] Likelihood ratio test for relaxation on Test branches, p = //' | awk -v group="$GROUP" 'BEGIN{OFS=" "} {print group, $0}' >> relax_pval.all
-	perl -p -e 's/\// /g' ${GROUP}_relax_K | perl -p -e 's/:/ /' | perl -p -e 's/\[RELAX\] Log\(L\) = \S+ Relaxation parameter K = //' | awk -v group="$GROUP" 'BEGIN{OFS=" "} {print group, $0}' >> relax_K.all   
+	perl -p -e 's/\// /g' ${GROUP}_pval | perl -p -e 's/:/ /' | perl -p -e 's/\[RELAX\] Likelihood ratio test for relaxation on Test branches, p = //' | awk -v group="$GROUP" 'BEGIN{OFS=" "} {print group, $0}' >> relax_pval.all
+	perl -p -e 's/\// /g' ${GROUP}_K | perl -p -e 's/:/ /' | perl -p -e 's/\[RELAX\] Log\(L\) = \S+ Relaxation parameter K = //' | awk -v group="$GROUP" 'BEGIN{OFS=" "} {print group, $0}' >> relax_K.all   
 done
