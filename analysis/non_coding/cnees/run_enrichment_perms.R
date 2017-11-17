df6dcc6522b6c167822d3bffc7932a9eed5159c8
Tim Sackton <tim@branta.local>
2017-11-17 15:11:22 -0500

    Error handling in enrich permutation code

--- analysis/non_coding/cnees/run_enrichment_perms.R
+++ analysis/non_coding/cnees/run_enrichment_perms.R
@@ -3,13 +3,14 @@ library(tidyverse)
 library(clusterProfiler)
 library(org.Gg.eg.db)
 library(parallel)
+library(rlist)
 
 #set working directory
 setwd("~/Projects/birds/ratite_compgen/ratite-genomics/analysis/non_coding/cnees/")
 
 #set cores and perms for whole run, can be individually modified later in code
-CORES <- 5
-PERMS <- 5
+CORES <- 4
+PERMS <- 8
 
 #load data
 cnee <- read_tsv("cnees.tsv")
@@ -42,6 +43,9 @@ input_counts<-list("set1" = cnee %>% filter(gene != ".", ratite_accel.1, ratite_
 #for each GO term in the merged go list, want to compute permutations: input is the merged term list and the counts
 
 get_go_perm <- function(DF, samples, golist, ont) {
+  if (sample(1:6) == 2) {
+    stop() 
+  }
   rand <- DF %>% sample_n(samples) %>% filter(gene != ".") %>% 
     dplyr::select(gene) %>% separate(gene, into=c("ncbi", "sym"), sep=":") %>% 
     distinct(ncbi)
@@ -56,17 +60,19 @@ get_go_perm <- function(DF, samples, golist, ont) {
 
 #to do one permutation of the full set
 get_one_perm_set <- function(perm, input, DF, golist, ont) {
-  lapply(input, get_go_perm, DF=DF, golist=golist, ont=ont) %>%
-  dplyr::bind_rows(.id="set")
+  try(lapply(input, get_go_perm, DF=DF, golist=golist, ont=ont) %>%
+  dplyr::bind_rows(.id="set"), TRUE)
 }
 
 #do permutations in parallel
 para_cores <- CORES
 num_perms <- PERMS
 
-perm_bp <- mclapply(1:num_perms, get_one_perm_set, input=input_counts, DF=cnee, golist=merged_bp_terms, ont="BP", mc.cores=para_cores, mc.preschedule = TRUE) %>%
+perm_bp <- mclapply(1:num_perms, get_one_perm_set, input=input_counts, DF=cnee, golist=merged_bp_terms, ont="BP", mc.cores=para_cores, mc.preschedule = FALSE) %>%
+  list.filter(class(.) == "data.frame") %>% 
   dplyr::bind_rows(.id="perm")
-perm_mf <- mclapply(1:num_perms, get_one_perm_set, input=input_counts, DF=cnee, golist=merged_mf_terms, ont="MF", mc.cores=para_cores, mc.preschedule = TRUE) %>%
+perm_mf <- mclapply(1:num_perms, get_one_perm_set, input=input_counts, DF=cnee, golist=merged_mf_terms, ont="MF", mc.cores=para_cores, mc.preschedule = FALSE) %>%
+  list.filter(class(.) == "data.frame") %>% 
   dplyr::bind_rows(.id="perm")
 
 #write out
@@ -104,10 +110,10 @@ gene_counts_obs <- lapply(c(quo(set1),quo(set2),quo(set3),quo(set4)), get_gene_c
 para_cores <- CORES
 num_perms <- PERMS
 
-gene_counts_perm <- bind_rows(list(set1 = mclapply(1:num_perms, perm_gene_counts, DF=cnee, indicator=quo(set1), mc.preschedule = TRUE, mc.cores = para_cores) %>% bind_rows(.id="perm"),
-                                   set2 = mclapply(1:num_perms, perm_gene_counts, DF=cnee, indicator=quo(set2), mc.preschedule = TRUE, mc.cores = para_cores) %>% bind_rows(.id="perm"),
-                                   set3 = mclapply(1:num_perms, perm_gene_counts, DF=cnee, indicator=quo(set3), mc.preschedule = TRUE, mc.cores = para_cores) %>% bind_rows(.id="perm"),
-                                   set4 = mclapply(1:num_perms, perm_gene_counts, DF=cnee, indicator=quo(set4), mc.preschedule = TRUE, mc.cores = para_cores) %>% bind_rows(.id="perm")), .id="set")
+gene_counts_perm <- bind_rows(list(set1 = mclapply(1:num_perms, perm_gene_counts, DF=cnee, indicator=quo(set1), mc.preschedule = FALSE, mc.cores = para_cores) %>% bind_rows(.id="perm"),
+                                   set2 = mclapply(1:num_perms, perm_gene_counts, DF=cnee, indicator=quo(set2), mc.preschedule = FALSE, mc.cores = para_cores) %>% bind_rows(.id="perm"),
+                                   set3 = mclapply(1:num_perms, perm_gene_counts, DF=cnee, indicator=quo(set3), mc.preschedule = FALSE, mc.cores = para_cores) %>% bind_rows(.id="perm"),
+                                   set4 = mclapply(1:num_perms, perm_gene_counts, DF=cnee, indicator=quo(set4), mc.preschedule = FALSE, mc.cores = para_cores) %>% bind_rows(.id="perm")), .id="set")
 
 write_tsv(gene_counts_perm, path="perm_gene_count_results.tsv")
 write_tsv(gene_counts_obs, path="obs_gene_count_results.tsv")
