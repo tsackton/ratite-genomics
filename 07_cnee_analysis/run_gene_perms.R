4f2d63ba39432a3f6bf8a75a3aac919b832bda40
Tim Sackton <tsackton@g.harvard.edu>
2018-10-24 20:27:46 -0400

    Updating gene permutation code
    Former-commit-id: 59d7b6cffb9d774fa74d0c880bfb8f63b0a2ed29

--- 07_cnee_analysis/run_gene_perms.R
+++ 07_cnee_analysis/run_gene_perms.R
@@ -14,7 +14,7 @@ perm_gene_counts <- function(perm, DF, indicator) {
   DF %>% filter(gene != ".") %>% mutate(rand = sample(!!indicator)) %>% count(gene, rand) %>% filter(!is.na(rand)) %>% spread(rand, n, fill=0, drop=FALSE, sep="_")
 }
 
-gene_gg4<-read_tsv("~/Projects/birds/ratite_compgen/ratite-genomics/04_wga/03_ce_annotation/cnees.galgal4.annotation", col_names = c("cnee", "gene"))
+gene_gg4<-read_tsv("../04_wga/03_ce_annotation/cnees.galgal4.annotation", col_names = c("cnee", "gene"))
 
 cnee_orig <- read_tsv("final_original_cnee.tsv.gz") %>% 
   select(version, cnee, logBF1, logBF2, it_pp_loss, ti_pp_loss, neo_tip_loss, floss_cl_pp, floss_cl_pp_dollo) %>%
@@ -54,21 +54,37 @@ cnee_ext2 <- read_tsv("final_extended_cnee.tsv.gz") %>%
   select(cnee, version, rar, crar, crar_dollo, gene)
 
 
-#make real dataset-- this needs editing
+#function to do work
 
-gene_counts_obs <- lapply(c(quo(set1),quo(set2),quo(set3),quo(set4)), get_gene_counts, DF=cnee) %>% bind_rows(.id="set")
+compute_gene_results <- function(DF, outname) {
+  
+  gene_perm_all <- list()
+  gene_res_all <- list()
+  
+  for (ver in c("gain", "gap", "gain_gap", "orig")) {
+    
+    cnee <- DF %>% filter(version == ver)
+    gene_res_all[[ver]] <- lapply(c(quo(rar),quo(crar),quo(crar_dollo)), get_gene_counts, DF=cnee) %>% bind_rows(.id="set")
+  
+    para_cores <- CORES
+    num_perms <- PERMS
+  
+    gene_counts_perm_list <- list(rar = mclapply(1:num_perms, perm_gene_counts, DF=cnee, indicator=quo(rar), mc.preschedule = FALSE, mc.cores = para_cores) %>% bind_rows(.id="perm"),
+                                crar = mclapply(1:num_perms, perm_gene_counts, DF=cnee, indicator=quo(crar), mc.preschedule = FALSE, mc.cores = para_cores) %>% bind_rows(.id="perm"),
+                                crar_dollo = mclapply(1:num_perms, perm_gene_counts, DF=cnee, indicator=quo(crar_dollo), mc.preschedule = FALSE, mc.cores = para_cores) %>% bind_rows(.id="perm"))
+  
+    gene_perm_all[[ver]] <- gene_counts_perm_list %>% bind_rows(.id="set")
+  
+  }
 
-para_cores <- CORES
-num_perms <- PERMS
+  bind_rows(gene_perm_all, .id="version") %>% write_tsv(paste0(outname, "_perm.tsv"))
+  bind_rows(gene_res_all, .id="version") %>% write_tsv(paste0(outname, "_real.tsv"))
+                                                                                                              
+}
 
-gene_counts_perm_list <- list(set1 = mclapply(1:num_perms, perm_gene_counts, DF=cnee, indicator=quo(set1), mc.preschedule = FALSE, mc.cores = para_cores) %>% bind_rows(.id="perm"),
-                              set2 = mclapply(1:num_perms, perm_gene_counts, DF=cnee, indicator=quo(set2), mc.preschedule = FALSE, mc.cores = para_cores) %>% bind_rows(.id="perm"),
-                              set3 = mclapply(1:num_perms, perm_gene_counts, DF=cnee, indicator=quo(set3), mc.preschedule = FALSE, mc.cores = para_cores) %>% bind_rows(.id="perm"),
-                              set4 = mclapply(1:num_perms, perm_gene_counts, DF=cnee, indicator=quo(set4), mc.preschedule = FALSE, mc.cores = para_cores) %>% bind_rows(.id="perm"))
+compute_gene_results(cnee_orig, "original_gene")
+compute_gene_results(cnee_ext, "extended_gene")
+compute_gene_results(cnee_ext2, "extended_ratiteVcorm_gene")
+compute_gene_results(cnee_red, "reduced_gene")
 
-gene_counts_perm <- gene_counts_perm_list %>% bind_rows(.id="set")
 
-write_tsv(gene_counts_perm, path="perm_gene_count_results.tsv")
-write_tsv(gene_counts_obs, path="obs_gene_count_results.tsv")
-
-#end GENE PERMUTATIONS#
