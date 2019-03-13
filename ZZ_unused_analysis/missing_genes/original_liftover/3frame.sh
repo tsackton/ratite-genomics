ff3307ba2ef929322322d2227982f47b0a14108e
Tim Sackton <tsackton@g.harvard.edu>
2019-03-13 10:33:45 -0400

    Removing unused files
    Former-commit-id: 2f36e6d12fbdb2ab672293a23a76b5dd1b6f95e5

--- ZZ_unused_analysis/missing_genes/original_liftover/3frame.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-for FILE in $(ls *.fa)
-do
-        echo "Submitting job for $FILE"
-        sbatch Stopsplitter.sbatch $FILE
-        sleep 1
-done
--- ZZ_unused_analysis/missing_genes/original_liftover/psl_submit.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-for FILE in $(ls *.fa)
-do
-	echo "Submitting job for $FILE"
-	sbatch psl_dict.sbatch $FILE
-	sleep 1
-done
