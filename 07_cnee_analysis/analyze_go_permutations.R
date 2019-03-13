6a8e52a6c5c79a2aada27c4137efd8e664001122
Tim Sackton <tsackton@g.harvard.edu>
2019-03-13 19:30:23 -0400

    Updating work on go permutation code
    Former-commit-id: e68c70747facef221137c7fcaed9c8ced7aada5b

--- 07_cnee_analysis/analyze_go_permutations.R
+++ 07_cnee_analysis/analyze_go_permutations.R
@@ -1,6 +1,8 @@
 #revised Nov 2018 for paper revisions
 library(tidyverse)
 
+path_to_data <- "~/Projects/birds/ratite_compgen/DRYAD/07_cnees/processed"
+
 transform_real <- function(DF) {
   DF %>% 
     separate(GeneRatio, into=c("target_in", "target_total")) %>% 
@@ -8,64 +10,56 @@ transform_real <- function(DF) {
     mutate(newpval = ifelse(is.na(pvalue), 1, pvalue), 
            logp = -log10(newpval),
            target_frac = as.numeric(target_in)/as.numeric(target_total), 
-           bg_frac = as.numeric(bg_in)/as.numeric(bg_total)) %>%
-    dplyr::select(version, set, ID, logp, target_frac, bg_frac) %>% 
+           bg_frac = as.numeric(bg_in)/as.numeric(bg_total),
+           enrich = log2(target_frac/bg_frac)) %>%
+    dplyr::select(version, set, ID, logp, target_frac, bg_frac, enrich) %>% 
+    filter(target_frac < 1, bg_frac < 1) %>%
     arrange(ID)
 }
 
 
-orig_bp <- read_tsv("~/Projects/birds/ratite_compgen/ratite-genomics/07_cnee_analysis/goperms/original_GO_galgal4_run1_BP_real.tsv") %>% transform_real()
-
-orig_mf <- read_tsv("~/Projects/birds/ratite_compgen/ratite-genomics/07_cnee_analysis/goperms/original_GO_galgal4_run1_MF_real.tsv") %>% transform_real()
+orig_bp <- read_tsv(paste0(path_to_data, "/original_GO_galgal4_run1_BP_real.tsv.gz")) %>% transform_real()
+orig_mf <- read_tsv(paste0(path_to_data, "/original_GO_galgal4_run1_MF_real.tsv.gz")) %>% transform_real()
 
 #read perms
-orig_bp_perm<-readRDS("~/Projects/birds/ratite_compgen/ratite-genomics/07_cnee_analysis/goperms/original_galgal4_BP.robj")
-orig_mf_perm<-readRDS("~/Projects/birds/ratite_compgen/ratite-genomics/07_cnee_analysis/goperms/original_galgal4_MF.robj")
+orig_bp_perm<-readRDS(paste0(path_to_data, "/goperms/original_galgal4_BP.robj"))
+orig_mf_perm<-readRDS(paste0(path_to_data, "/goperms/original_galgal4_MF.robj"))
+
+orig_bp_merge <- full_join(orig_bp, orig_bp_perm, by=c("version" = "version", "set" = "set", "ID" = "ID")) 
+orig_mf_merge <- full_join(orig_mf, orig_mf_perm, by=c("version" = "version", "set" = "set", "ID" = "ID"))
 
 #compute P-values
 
-orig_bp_merge <- full_join(orig_bp, orig_bp_perm, by=c("version" = "version", "set" = "set", "ID" = "ID")) %>% 
+orig_bp_merge <-
+  orig_bp_merge %>% 
   rowwise %>% 
   mutate(pval_frac = max(1-ecdf_frac(target_frac), 1e-04), 
          pval_logp = max(1-ecdf_logp(logp), 1e-04), 
-         pval_enrich = max(1-ecdf_enrich(log2(target_frac/bg_frac))), 1e-04) %>% 
+         pval_enrich = max(1-ecdf_enrich(enrich), 1e-04)) %>% 
   ungroup %>% group_by(version, set) %>% 
   mutate(qval_logp = p.adjust(pval_logp, "BH"),
          qval_frac = p.adjust(pval_frac, "BH"),
          qval_enrich = p.adjust(pval_enrich, "BH"))
 
-
-orig_mf_merge <- full_join(orig_mf, orig_mf_perm, by=c("version" = "version", "set" = "set", "ID" = "ID")) %>% 
+orig_mf_merge <-
+  orig_mf_merge %>% 
   rowwise %>% 
   mutate(pval_frac = max(1-ecdf_frac(target_frac), 1e-04), 
          pval_logp = max(1-ecdf_logp(logp), 1e-04), 
-         pval_enrich = max(1-ecdf_enrich(log2(target_frac/bg_frac)), 1e-04)) %>% 
+         pval_enrich = max(1-ecdf_enrich(enrich), 1e-04)) %>% 
   ungroup %>% group_by(version, set) %>% 
   mutate(qval_logp = p.adjust(pval_logp, "BH"),
          qval_frac = p.adjust(pval_frac, "BH"),
          qval_enrich = p.adjust(pval_enrich, "BH"))
 
-orig_mf_merge %>% filter(version == "gain") %>% filter(qval_frac < 0.20) %>% 
-  select(set, ID, target_frac, bg_frac, pval_frac, qval_frac) %>% 
-  write_tsv("~/Projects/birds/ratite_compgen/manuscript/ScienceSubmissionRev2/orig_gain_mf_results_2.tsv")
-  
-#ugly hack
-orig_mf_merge %>% filter(version == "gain") %>% filter(qval_frac < 0.20) %>% pull(ecdf_frac) %>% map(., summary) %>% map_dbl(., 3)
-  
-  
-orig_bp_merge %>% filter(version == "gain") %>% filter(qval_frac < 0.20)  %>% 
-  select(set, ID, target_frac, bg_frac, pval_frac, qval_frac)  %>% 
-  write_tsv("~/Projects/birds/ratite_compgen/manuscript/ScienceSubmissionRev2/orig_gain_bp_results_2.tsv")
+#analysis
 
-#ugly hack
-orig_bp_merge %>% filter(version == "gain") %>% filter(qval_frac < 0.20) %>% pull(ecdf_frac) %>% map(., summary) %>% map_dbl(., 3) %>% as.data.frame() %>%
-  write_tsv("~/Projects/birds/ratite_compgen/manuscript/ScienceSubmissionRev2/orig_gain_bp_results_2-etf.tsv")
+orig_mf_merge %>% filter(version == "gain") %>% filter(qval_frac < 0.25) %>% 
+  mutate(exp_frac = map(ecdf_frac, summary) %>% map_dbl(., 3)) %>%
+  select(set, ID, target_frac, exp_frac, bg_frac, enrich, pval_frac, qval_frac) %>% 
+  write_tsv(paste0(path_to_data, "/GOPERM_orig_gain_mf_results.tsv"))
 
-
-
-summary((orig_mf_merge %>% filter(ID == "GO:0003676", version == "gain", set == "crar") %>% pull(ecdf_frac))[[1]])
-summary((orig_mf_merge %>% filter(ID == "GO:0003677", version == "gain", set == "crar") %>% pull(ecdf_frac))[[1]])
-summary((orig_mf_merge %>% filter(ID == "GO:0043565", version == "gain", set == "crar") %>% pull(ecdf_frac))[[1]])
-summary((orig_bp_merge %>% filter(ID == "GO:0006355", version == "gain", set == "crar") %>% pull(ecdf_frac))[[1]])
-summary((orig_bp_merge %>% filter(ID == "GO:0060173", version == "gain", set == "crar") %>% pull(ecdf_frac))[[1]])
-
+orig_bp_merge %>%filter(version == "gain") %>% filter(qval_frac < 0.25) %>% 
+  mutate(exp_frac = map(ecdf_frac, summary) %>% map_dbl(., 3)) %>%
+  select(set, ID, target_frac, exp_frac, bg_frac, enrich, pval_frac, qval_frac) %>% 
+  write_tsv(paste0(path_to_data, "/GOPERM_orig_gain_bp_results.tsv"))
