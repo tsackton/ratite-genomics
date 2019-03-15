d7555c89aa2ecadb88a5dd180264b9cbd5244308
Tim Sackton <tsackton@g.harvard.edu>
2019-03-15 12:21:38 -0400

    Updating code for supplemental figures
    Former-commit-id: 6a9987024297ebe708b1746dccc918771ecc6996

---
+++ 07_cnee_analysis/REVIGO.r
@@ -0,0 +1,65 @@
+
+
+# A plotting R script produced by the REVIGO server at http://revigo.irb.hr/
+# If you found REVIGO useful in your work, please cite the following reference:
+# Supek F et al. "REVIGO summarizes and visualizes long lists of Gene Ontology
+# terms" PLoS ONE 2011. doi:10.1371/journal.pone.0021800
+
+
+# --------------------------------------------------------------------------
+# If you don't have the ggplot2 package installed, uncomment the following line:
+# install.packages( "ggplot2" );
+library( ggplot2 );
+# --------------------------------------------------------------------------
+# If you don't have the scales package installed, uncomment the following line:
+# install.packages( "scales" );
+library( scales );
+
+
+# --------------------------------------------------------------------------
+# Here is your data from REVIGO. Scroll down for plot configuration options.
+
+revigo.names <- c("term_ID","description","frequency_%","plot_X","plot_Y","plot_size","value","uniqueness","dispensability");
+revigo.data <- rbind(c("GO:0050680","negative regulation of epithelial cell proliferation", 0.409, 3.106, 5.014, 1.690, 3.2054,0.497,0.000),
+c("GO:0035108","limb morphogenesis", 0.673,-5.654, 1.701, 1.903, 2.6047,0.280,0.026),
+c("GO:0016055","Wnt signaling pathway", 1.764, 2.880,-4.795, 2.318, 1.7359,0.520,0.079),
+c("GO:0048736","appendage development", 0.886,-5.719,-1.082, 2.021, 2.6428,0.513,0.290));
+
+one.data <- data.frame(revigo.data, stringsAsFactors = FALSE);
+names(one.data) <- revigo.names;
+one.data <- one.data [(one.data$plot_X != "null" & one.data$plot_Y != "null"), ];
+one.data$plot_X <- as.numeric( as.character(one.data$plot_X) );
+one.data$plot_Y <- as.numeric( as.character(one.data$plot_Y) );
+one.data$plot_size <- as.numeric( as.character(one.data$plot_size) );
+one.data$frequency <- as.numeric( as.character(one.data$frequency) );
+one.data$uniqueness <- as.numeric( as.character(one.data$uniqueness) );
+one.data$dispensability <- as.numeric( as.character(one.data$dispensability) );
+one.data$value <- as.numeric(as.character(one.data$value))
+#head(one.data);
+
+
+# --------------------------------------------------------------------------
+# Names of the axes, sizes of the numbers and letters, names of the columns,
+# etc. can be changed below
+
+p1 <- ggplot( data = one.data );
+p1 <- p1 + geom_point( aes( plot_X, plot_Y, colour = plot_size, size = value), alpha = I(0.6) ) + scale_size_area() + scale_color_gradient();
+p1 <- p1 + scale_size( range=c(5, 30)) + theme_bw(); # + scale_fill_gradientn(colours = heat_hcl(7), limits = c(-300, 0) );
+ex <- one.data [ one.data$dispensability < 0.9, ]; 
+p1 <- p1 + geom_text( data = ex, aes(plot_X, plot_Y, label = description), colour = I(alpha("black", 0.85)), size = 3 );
+p1 <- p1 + labs (y = "semantic space x", x = "semantic space y");
+p1 <- p1 + theme(legend.key = element_blank()) ;
+one.x_range = max(one.data$plot_X) - min(one.data$plot_X);
+one.y_range = max(one.data$plot_Y) - min(one.data$plot_Y);
+p1 <- p1 + xlim(min(one.data$plot_X)-one.x_range/10,max(one.data$plot_X)+one.x_range/10);
+p1 <- p1 + ylim(min(one.data$plot_Y)-one.y_range/10,max(one.data$plot_Y)+one.y_range/10);
+
+# --------------------------------------------------------------------------
+# Output the plot to screen
+
+p1;
+
+# Uncomment the line below to also save the plot to a file.
+# The file type depends on the extension (default=pdf).
+
+ggsave("revigo-plot.pdf");
